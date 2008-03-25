42b8bffec14743f7308c7dd03c0a913f109d3906
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2008-03-25 17:31:05 +0000

    + a script to find files in directories specified by PATH environment variable.
    git-svn-id: https://jff.googlecode.com/svn/trunk@24 1de5394f-e846-0410-8a38-9b14b2994ffe

---
+++ utils/whereis.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl -w
+#
+# Find files in directories specified by PATH environment variable.
+#
+use strict;
+use warnings;
+use Cwd;
+
+die "Usage: whereis.pl glob_pattern\n  eg.\twhereis.pl \"gcc*\"\n" if @ARGV != 1;
+
+my $sep = ':';
+my $dir = '/';
+
+if ($^O =~ /MSWin32/i) {
+    $sep = ';';
+    $dir = '\\';
+}
+
+my @paths = split $sep, $ENV{PATH};
+my @expands;
+my $cwd;
+for (@paths) {
+    $cwd = cwd();
+    chdir $_ || die "Can't chdir($_): $!\n";
+    @expands = glob($ARGV[0]);
+    # if expansion fails, glob() returns the pattern, so @expands == 1
+    if (@expands > 0 && -e $expands[0]) {
+        print $_, "\n\t", join("\n\t", @expands), "\n";
+    }
+    chdir $cwd || die "Can't chdir($cwd): $!\n";
+}
+
