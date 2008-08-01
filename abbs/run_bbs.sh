85f283d7de4ea49fdead928e2b706edf74d15beb
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2008-08-01 01:59:22 +0000

    polish autotools support
    git-svn-id: https://jff.googlecode.com/svn/trunk@78 1de5394f-e846-0410-8a38-9b14b2994ffe

--- abbs/run_bbs.sh
+++ abbs/run_bbs.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-LD_LIBRARY_PATH=. ./bbs for_test/myboard for_test/pool `pwd`/for_test/tmp
+set -x
+
+./bbs for_test/myboard for_test/pool `pwd`/for_test/tmp
 
--- abbs/run_boardd.sh
+++ abbs/run_boardd.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+set -x
+
 mkdir -p for_test/pool for_test/tmp 2>/dev/null
 
-LD_LIBRARY_PATH=. ./boardd for_test/myboard for_test/pool
+./boardd for_test/myboard for_test/pool
 
