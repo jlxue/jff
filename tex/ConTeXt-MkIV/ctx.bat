ad3d50f632026ff33a0d17dde61a3a814b942892
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2008-10-15 02:53:41 +0000

    + 增加  ConTeXt MkIV 中文文档模板，从 Li Yanrui 的 ctxnotes 修改而来。
    git-svn-id: https://jff.googlecode.com/svn/trunk@107 1de5394f-e846-0410-8a38-9b14b2994ffe

--- tex/ConTeXt-MkIV/ctx.bat
+++ tex/ConTeXt-MkIV/ctx.bat
@@ -1,4 +1,4 @@
 set path=c:\ruby\bin;%path%
-E:\dev\context\tex\setuptex.bat E:\dev\context\tex
 set OSFONTDIR=E:\jff\tex\ConTeXt-MkIV\Adobe-Fonts;C:\Windows\Fonts
+E:\dev\context\tex\setuptex.bat E:\dev\context\tex
 
