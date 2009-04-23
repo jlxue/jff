cc4015a341f5603fb2727b6e5325428e02acf425
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2009-04-23 06:28:22 +0000

    * 修正 ssh-agent 已经杀死导致 ps -p $SSH_AGENT_PID -o uid= 返回空而 [ $uid -ne "" ] 出错的问题
    git-svn-id: https://jff.googlecode.com/svn/trunk@184 1de5394f-e846-0410-8a38-9b14b2994ffe

--- utils/auto-start-ssh-agent.sh
+++ utils/auto-start-ssh-agent.sh
@@ -1,16 +1,17 @@
 #### start ssh-agent automatically when login
-##
+##  v1.0
 ## add this code snippet to /etc/profile or $HOME/{.profile,.bash_profile,.bashrc}
 
-[ -r $HOME/.ssh-agent-info ] && source $HOME/.ssh-agent-info
+[ -r $HOME/.ssh-agent-info ] && . $HOME/.ssh-agent-info
 
 uid=`id -u`
+[ -z "$SSH_AGENT_PID" ] && agent_uid= || agent_uid=`ps -p $SSH_AGENT_PID -o uid=`
 
-if [ -z "$SSH_AGENT_PID" ] || [ "$uid" -ne "`ps -p $SSH_AGENT_PID -o uid=`" ]; then
+if [ -z "$agent_uid" ] || [ "$uid" -ne "$agent_uid" ]; then
     killall -u "$USER" ssh-agent 2>/dev/null
 
     ssh-agent > $HOME/.ssh-agent-info
-    [ -r $HOME/.ssh-agent-info ] && source $HOME/.ssh-agent-info
+    [ -r $HOME/.ssh-agent-info ] && . $HOME/.ssh-agent-info
 fi
 
 if [ -n "$SSH_AGENT_PID" ]; then
@@ -20,6 +21,7 @@ if [ -n "$SSH_AGENT_PID" ]; then
 fi
 
 unset uid
+unset agent_uid
 
 ssh-add -l >/dev/null || ssh-add
 
