cd7f532a93cd0a86e9f056eca291eb14dd82d9a7
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2009-09-12 22:11:52 +0000

    don't use zhwinfonts.tex because pdflatex has trouble with it
    git-svn-id: https://jff.googlecode.com/svn/trunk@272 1de5394f-e846-0410-8a38-9b14b2994ffe

---
+++ tex/TeXLive/templates/all.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir t || exit 1
+cd t || exit 1
+
+cp ../gbk.tex gbk-cjkfonts.tex
+cp ../gbk.tex gbk-winfonts.tex
+cp ../gbk.tex gbk-adobefonts.tex
+
+cp ../utf8.tex utf8-cjkfonts.tex
+cp ../utf8.tex utf8-winfonts.tex
+cp ../utf8.tex utf8-adobefonts.tex
+
+sed -i -e 's/\<winfonts\>/cjkfonts/' gbk-cjkfonts.tex utf8-cjkfonts.tex
+
+sed -i -e 's/\<winfonts\>/adobefonts/' gbk-adobefonts.tex utf8-adobefonts.tex
+
+for f in *.tex; do
+    for t in xelatex pdflatex dvipdfmx dvips; do
+        make -f ../Makefile TOOLCHAINS=$t,$t `basename $f .tex`
+    done
+done
+
