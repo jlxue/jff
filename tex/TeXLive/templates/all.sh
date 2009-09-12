4e747305d218952a1a3192fe6d55508c682acb5e
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2009-09-12 22:14:26 +0000

    use dvips back driver for hyperref if compiled with dvips
    git-svn-id: https://jff.googlecode.com/svn/trunk@283 1de5394f-e846-0410-8a38-9b14b2994ffe

--- tex/TeXLive/templates/all.sh
+++ tex/TeXLive/templates/all.sh
@@ -16,8 +16,11 @@ sed -i -e 's/\<winfonts\>/cjkfonts/' gbk-cjkfonts.tex utf8-cjkfonts.tex
 sed -i -e 's/\<winfonts\>/adobefonts/' gbk-adobefonts.tex utf8-adobefonts.tex
 
 for f in *.tex; do
-    for t in xelatex pdflatex dvipdfmx dvips; do
+    for t in xelatex pdflatex dvipdfmx; do
         make -f ../Makefile TOOLCHAINS=$t,$t `basename $f .tex`
     done
+
+    sed -i -e 's/\<dvipdfmx\>/ps2pdf/' $f
+    make -f ../Makefile TOOLCHAINS=dvips,dvips `basename $f .tex`
 done
 
