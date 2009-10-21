6216b3df0d017b7aaf501c51afb36b216811b1ef
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2009-10-21 10:37:54 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@334 1de5394f-e846-0410-8a38-9b14b2994ffe

--- dot_files/emacs/dot_emacs.d/site-start.d/04_plugins.el
+++ dot_files/emacs/dot_emacs.d/site-start.d/04_plugins.el
@@ -10,3 +10,13 @@
 ;; linum.el
 (require 'linum)
 
+;; easypg
+(require 'epa-file)
+(epa-file-enable)
+(setq epa-file-encrypt-to nil)
+(setq epa-file-cache-passphrase-for-symmetric-encryption t)
+(setq epa-file-inhibit-auto-save nil)
+
+;; ccrypt
+;(require 'jka-compr-ccrypt)
+
---
+++ utils/fontshow.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+#
+# Usage:
+#
+# fc-list :lang=zh | cut -d, -f1 | cut -d: -f1 | perl fontshow.pl 14 hello world 中国人民你好 > a.html
+# firefox a.html
+#
+use strict;
+use warnings;
+
+my ($size, @strings) = @ARGV;
+
+if (@strings == 0) {
+    print STDERR "Usage: $0 font-size string... < fonts-list";
+    exit 1;
+}
+
+while (<STDIN>) {
+    chomp;
+    print "<font size=\"$size\" face=\"$_\">@strings $_:$size</font><br/>\n";
+}
+
