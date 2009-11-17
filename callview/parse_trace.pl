ec9eb4cdd8038b90cd3051bfaf76b8a1876e1b2d
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2009-11-17 08:05:47 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@358 1de5394f-e846-0410-8a38-9b14b2994ffe

--- callview/parse_trace.pl
+++ callview/parse_trace.pl
@@ -3,22 +3,25 @@ use strict;
 use warnings;
 
 my $record;
-my ($exited, $addr);
 my %h = ();
+my @a = ();
 
 open F, "$ARGV[0]" or die "$!\n";
 binmode F;
 
-while (sysread(F, $record, 5)) {
-    ($exited, $addr) = unpack "cL", $record;
+while (sysread(F, $record, 5 * 1024 * 8)) {
+    @a = unpack "(cL)*", $record;
+    for (my $i = 0; $i < @a; $i += 2) {
+        next if $a[$i];
 
-    next if $exited;
-
-    $h{$addr}++;
+        $h{$a[$i+1]}++;
+    }
 }
+undef @a;
+undef $record;
 
 while (my ($k, $v) = each %h) {
-    print "$k $v\n";
+    printf "%08x %d\n", $k, $v;
 }
 
 close F;
