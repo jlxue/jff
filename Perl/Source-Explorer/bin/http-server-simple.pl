6b06d32ca57b0cb410f78826c5ab26edaa13fdca
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2009-12-30 17:39:12 +0000

    a preforked http server written with HTTP::Server::Simple
    git-svn-id: https://jff.googlecode.com/svn/trunk@413 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/Source-Explorer/bin/http-server-simple.pl
+++ Perl/Source-Explorer/bin/http-server-simple.pl
@@ -1,9 +1,24 @@
 #!/usr/bin/perl
-use HTTP::Server::Simple;
 use strict;
 use warnings;
+use constant NET_SERVER => 'Net::Server::PreForkSimple';
 
-my $server = new HTTP::Server::Simple;
-
+my $server = new MyServer();
 $server->run();
+exit(0);
+
+#=========================================================
+package MyServer;
+use base qw(HTTP::Server::Simple::CGI);
+use HTTP::Server::Simple::Static;
+
+sub handle_request {
+    my ($self, $cgi) = @_;
+    return $self->serve_static($cgi, '/');
+}
+
+
+sub net_server {
+    return ::NET_SERVER;
+}
 
