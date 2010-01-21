1fe7d467aae951f30f162b8afa6afc921af6cb9e
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-01-21 10:37:09 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@450 1de5394f-e846-0410-8a38-9b14b2994ffe

--- XDE/xde/bin/update-menu.pl
+++ XDE/xde/bin/update-menu.pl
@@ -14,17 +14,24 @@ XML::SAX::ParserFactory->parser(
 exit 0;
 
 ###############################################################
-{
+{ # begin package
 package MenuXMLHandler;
 use base qw/XML::SAX::Base/;
+use Class::Struct Menu => [parent   => 'Menu',
+                           children => '*@',
+                           name     => '$',
+                           directory=> '$',
+                          ];
+use Data::Dumper;
 use Smart::Comments;
+use strict;
+use warnings;
 
-my %element_handlers = (
-    Menu        =>  \&handle_menu;
-);
 
 sub start_document {
-    ### start_document: @_
+    my $self = shift;
+    $self->{root_menu} = new Menu;
+    $self->{current_menu} = $self->{root_menu};
 }
 
 sub end_document {
@@ -36,26 +43,71 @@ sub start_element {
     my $localname = $e->{LocalName};
 
     $self->{characters} = '';
-    $element_handlers{$localname}->(@_) if exists $element_handlers{$localname};
+
+    my $sub = $self->can("handle_start_$localname");
+    $sub->(@_) if defined $sub;
 }
 
 sub end_element {
-    ### end_element: @_
+    my ($self, $e) = @_;
+    my $localname = $e->{LocalName};
+
+    my $sub = $self->can("handle_end_$localname");
+    $sub->(@_) if defined $sub;
 }
 
 sub characters {
-    ### characters: @_
+    $_[0]{characters} .= $_[1]{Data};
 }
 
-}
-
-sub handle_menu {
+sub handle_start_Menu {
     my ($self, $e) = @_;
+    my $parent = $self->{current_menu};
+    my $menu = new Menu(parent => $parent);
 
-    if (exists $self->{parent_menus}) {
-    } else {
-        $self->{root_menu} = {};
-        $self->{parent_menus} = [];
-    }
+    push @{$parent->children()}, $menu;
+    $self->{current_menu} = $menu;
 }
+
+sub handle_end_Menu {
+    $_[0]{current_menu} = $_[0]{current_menu}->parent;
+}
+
+sub handle_end_Name {
+    $_[0]{current_menu}->name($_[0]->{characters});
+}
+
+sub handle_end_Directory {
+    $_[0]{current_menu}->directory($_[0]->{characters});
+}
+
+sub handle_start_Include {
+    $_[0]{operators} = [];
+    $_[0]{operands} = [];
+    $_[0]{expression} = "";
+}
+
+sub handle_end_Include {
+}
+
+sub handle_start_And {
+    push @{$_[0]{operators}}, 'Or';
+    push @{$_[0]{operands}}, 'Or';
+}
+
+sub handle_start_Not {
+    push @{$_[0]{operators}}, 'Not';
+}
+
+sub handle_end_Category {
+    push @{$_[0]{operators}}, 'Category';
+    push @{$_[0]{operands}}, $_[0]{characters};
+}
+
+sub handle_end_Filename {
+    push @{$_[0]{operators}}, 'Filename';
+    push @{$_[0]{operands}}, $_[0]{characters};
+}
+
+} # end package
 
