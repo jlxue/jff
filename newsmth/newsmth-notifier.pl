361a85f19d3cae1b46468508e55271e981e04f82
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-02-01 08:48:18 +0000

    newsmth-notifier.pl v3.0
    git-svn-id: https://jff.googlecode.com/svn/trunk@467 1de5394f-e846-0410-8a38-9b14b2994ffe

--- newsmth/newsmth-notifier.pl
+++ newsmth/newsmth-notifier.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl
 #
 #  newsmth-notifier.pl - poll newsmth.net for new messages
 #
@@ -17,7 +17,7 @@
 #         (这个脚本必须是 GBK 编码)
 #
 #  ChangeLog:
-#       2007-10-03      
+#       2007-10-03
 #           * can poll newsmth for new messages
 #           * release 1.0
 #       2007-10-05
@@ -28,6 +28,9 @@
 #       2009-01-14
 #           * fix GBK/UTF-8 terminal encoding problem
 #           * support system tray icon for Windows, release 2.0
+#       2010-02-01
+#           * cleanup code, support desktop notification for Linux
+#             release 3.0
 #
 #  Dieken at newsmth.net
 #
@@ -35,176 +38,45 @@
 #
 #  TODO: cleanup code, port to Linux
 #
-use strict;
-use warnings;
-use Encode;
-use File::Slurp;
-use LWP::Simple;
-use POSIX qw/setsid strftime/;
-#use Proc::Daemon;
-use threads;
-use threads::shared;
-use Thread::Queue;
-use Win32::GUI ();
-use constant WM_NOTIFYICON => 40000;
-
-$| = 1;
-my $terminal_encoding;
-if ($^O eq "MSWin32" || 
-        (exists($ENV{'LC_MESSAGES'}) && $ENV{'LC_MESSAGES'} =~ /\.GB(?:2312|K|18030)/i) ||
-        (exists($ENV{'LANG'})        && $ENV{'LANG'}        =~ /\.GB(?:2312|K|18030)/i) ||
-        (exists($ENV{'LC_CTYPE'})    && $ENV{'LC_CTYPE'}    =~ /\.GB(?:2312|K|18030)/i)) {
-    $terminal_encoding = 'GBK';
-} else {
-    $terminal_encoding = 'UTF-8';
-}
-
-my %lastPostIds;
-my $got_sig_hup = 0;
-
-load_board_list();
-
-$SIG{HUP}  = sub { ++$got_sig_hup; };
-
-# daemonize();
-
-
-$SIG{INT}  = \&save_board_list;
-$SIG{KILL} = \&save_board_list;
-$SIG{QUIT} = \&save_board_list;
-
-my $q = new Thread::Queue;
-my $thread = threads->create(\&systray_thread);
-$thread->detach();
-$q->dequeue();      # When systray_thread is ready it will give me a message.
-
-while ($thread->is_running()) {
-    if ($got_sig_hup > 0) {
-        load_board_list();
-        --$got_sig_hup;
-    }
-
-    print "# checking...........................\n";
-    my $message = "";
-
-    for my $board (sort(keys %lastPostIds)) {
-        my $count = 0;
-
-        my $content = get('http://www.newsmth.net/bbsdoc.php?board=' . $board);
-        next unless defined $content;
-
-        my @posts = parse_page($content);
-        
-        for my $post (@posts) {
-            if (exists $lastPostIds{$board}) {
-                if ($lastPostIds{$board} < $post->[0]) {
-                    $lastPostIds{$board} = $post->[0];
-                    print $board, ': ', format_post($post), "\n";
-                    ++$count;
-                }
-            } else {
-                $lastPostIds{$board} = $post->[0];
-            }
-        }
-
-        $message .= sprintf("%16s %d\n", $board, $count) if $count > 0;
-
-        sleep rand 5;
-    }
-
-    $q->enqueue($message);
-    sendmessage();
-
-    save_board_list();
-    sleep 60 if $thread->is_running();
-}
-
-
-END {
-    save_board_list();
-}
-
-############################################################
-#
-sub parse_page {
-    my $content = shift;
-    #my ($postId, $topicId, $author, $flags, $time, $title, $bytes, $n);
-    my (@a, @result);
-    my ($begin, $end) = (0, 0);
-    while (($begin = index($content, 'c.o(', $begin)) >= 0) {
-        $end = index($content, ");\n", $begin);
-        last if $end <= $begin;
-        @a = substr($content, $begin + 4, $end - $begin) =~
-                /(\d+),(\d+),'(.*)','(.*)',(\d+),'(.*)',(\d+),(\d+)/;
-        push @result, [@a];
-        $begin = $end;
-    }
-
-    return @result;
-}
-
-
-sub format_post {
-    my $post = shift;
-    my $result = sprintf "[%10s] %6d (%-12s) | %s",
-                         strftime("%m-%d %H:%M", localtime($post->[4])), # time
-                         $post->[0],    # postId
-                         $post->[2],    # author
-                         $post->[5];    # title
-
-    if ('GBK' ne $terminal_encoding) {
-        Encode::from_to($result, 'GBK', $terminal_encoding);
-    }
-
-    return $result;
-}
-
-
-sub daemonize {
-    # http://www.newsmth.net/bbscon.php?bid=226&id=15888
-    defined(my $pid = fork) or die "Can't fork: $!\n";
-    exit if $pid;
-    open STDIN, '/dev/null' or die "Can't read /dev/null: $!\n";
-    open STDOUT, '>/dev/null' or die "Can't write stdout to /dev/null: $!\n";
-    open STDERR, '>/dev/null' or die "Can't write stderr to /dev/null: $!\n";
-    chdir '/' or die "Can't chdir to /: $!\n";
-    umask 0;
-    setsid or die "Can't start a new session: $!\n";
-}
-
-
-sub load_board_list {
-    open (my $fh, 'board.list') || die "Can't read board list: $!\n";
-    while (<$fh>) {
-        chomp;
-        next if /^\s*#/;
-        s/^\s+//;
-        my ($board, $id) = split /\s+/;
-        $lastPostIds{$board} = ($id || 0) if defined $board;
-    }
-    close $fh;
-}
-
-
-sub save_board_list {
-    my $sig = shift;
-    local $SIG{$sig} = 'IGNORE' if defined $sig;
-
-    my @a;
-    push @a, sprintf("%-32s        %s\n", "# board name", "last post id");
-    while (my ($board, $id) = each %lastPostIds) {
-        push @a, sprintf("%-32s        %d\n", $board, $id);
-    }
-    write_file('board.list', {binmode => ':utf8', atomic => 1}, \@a);
-
-    exit 0 if defined $sig;
-}
-
 
 #########################################################
 ## system tray notification
-#
-my $main;
+
+{ # begin package WindowsNotify
+package WindowsNotify;
+
+use threads;
+use threads::shared;
+use Thread::Queue;
+use if $^O eq "MSWin32", Win32::GUI => ();
+use constant WM_NOTIFYICON => 40000;
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT = qw(notify_start notify_alive notify_send notfy_stop);
+our @EXPORT_OK = qw(notify_start notify_alive notify_send notfy_stop);
+
+my ($main, $q, $thread);
+
+sub notify_start {
+    $q = new Thread::Queue;
+    $thread = threads->create(\&systray_thread);
+    $thread->detach();
+    $q->dequeue();      # When systray_thread is ready it will give me a message.
+}
+
+sub notify_alive {
+    $thread->is_running();
+}
+
+sub notify_send {
+    $q->enqueue($_[0]);
+    sendmessage();
+}
+
+sub notify_stop {
+}
 
 sub systray_thread {
 
@@ -274,4 +146,243 @@ sub notify_new_articles {
 
     return 1;
 }
+} # end package WindowsNotify
+
+
+{ # begin package LinuxNotify
+package LinuxNotify;
+
+use if $^O ne 'MSWin32', "Desktop::Notify";        # require to install notification-daemon
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT = qw(notify_start notify_alive notify_send notfy_stop);
+our @EXPORT_OK = qw(notify_start notify_alive notify_send notfy_stop);
+
+my ($notify, $notification);
+
+sub notify_start {
+    $notify = Desktop::Notify->new();
+    $notification = $notify->create(summary => 'NewSMTH Notifier',
+                                    body    => "I'm ready, go!",
+                                    timeout => 5000);
+    $notification->show();
+}
+
+sub notify_alive {
+    1;
+}
+
+sub notify_send {
+    $notification->body($_[0]);
+    $notification->show();
+}
+
+sub notify_stop {
+    $notification->close();
+}
+
+} # end package LinuxNotify
+
+{ # begin package DummyNotify
+package DummyNotify;
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT = qw(notify_start notify_alive notify_send notfy_stop);
+our @EXPORT_OK = qw(notify_start notify_alive notify_send notfy_stop);
+
+sub notify_start {
+}
+
+sub notify_alive {
+    1;
+}
+
+sub notify_send {
+}
+
+sub notify_stop {
+}
+
+} # end package DummyNotify
+
+#########################################################
+{   # begin package main
+use strict;
+use warnings;
+use Encode;
+use File::Slurp;
+use LWP::Simple;
+use POSIX qw/setsid strftime/;
+#use Proc::Daemon;
+
+if ($^O eq 'MSWin') {
+    WindowsNotify->import();
+} elsif ($^O eq 'linux') {
+    LinuxNotify->import();
+} else {
+    warn "use dummy notification implementation!\n";
+    DummyNotify->import();
+}
+
+
+$| = 1;
+my $terminal_encoding;
+if ($^O eq "MSWin32" ||
+        (exists($ENV{'LC_MESSAGES'}) && $ENV{'LC_MESSAGES'} =~ /\.GB(?:2312|K|18030)/i) ||
+        (exists($ENV{'LANG'})        && $ENV{'LANG'}        =~ /\.GB(?:2312|K|18030)/i) ||
+        (exists($ENV{'LC_CTYPE'})    && $ENV{'LC_CTYPE'}    =~ /\.GB(?:2312|K|18030)/i)) {
+    $terminal_encoding = 'GBK';
+} else {
+    $terminal_encoding = 'UTF-8';
+    binmode STDOUT, ":utf8";
+    binmode STDERR, ":utf8";
+}
+
+my %lastPostIds;
+my $got_sig_hup = 0;
+
+load_board_list();
+
+$SIG{HUP}  = sub { ++$got_sig_hup; };
+
+# daemonize();
+
+
+$SIG{INT}  = \&save_board_list;
+$SIG{KILL} = \&save_board_list;
+$SIG{QUIT} = \&save_board_list;
+
+notify_start();
+
+while (notify_alive()) {
+    if ($got_sig_hup > 0) {
+        load_board_list();
+        --$got_sig_hup;
+    }
+
+    print "# checking...........................\n";
+    my $message = "";
+
+    for my $board (sort(keys %lastPostIds)) {
+        my $count = 0;
+
+        # At least in in LWP v5.827  get() returns decoded text, so $context is
+        # encoded in Perl's internal encoding(UTF-8).   2010-02-10 lyb
+        my $content = get('http://www.newsmth.net/bbsdoc.php?board=' . $board);
+        next unless defined $content;
+
+        my @posts = parse_page($content);
+
+        for my $post (@posts) {
+            if (exists $lastPostIds{$board}) {
+                if ($lastPostIds{$board} < $post->[0]) {
+                    $lastPostIds{$board} = $post->[0];
+                    print $board, ': ', format_post($post), "\n";
+                    ++$count;
+                }
+            } else {
+                $lastPostIds{$board} = $post->[0];
+            }
+        }
+
+        $message .= sprintf("%16s %d\n", $board, $count) if $count > 0;
+
+        sleep rand 5;
+    }
+
+    notify_send($message);
+
+    save_board_list();
+    sleep 60 if notify_alive();
+}
+
+
+END {
+    save_board_list();
+}
+
+############################################################
+#
+sub parse_page {
+    my $content = shift;
+    #my ($postId, $topicId, $author, $flags, $time, $title, $bytes, $n);
+    my (@a, @result);
+    my ($begin, $end) = (0, 0);
+    while (($begin = index($content, 'c.o(', $begin)) >= 0) {
+        $end = index($content, ");\n", $begin);
+        last if $end <= $begin;
+        @a = substr($content, $begin + 4, $end - $begin) =~
+                /(\d+),(\d+),'(.*)','(.*)',(\d+),'(.*)',(\d+),(\d+)/;
+        push @result, [@a];
+        $begin = $end;
+    }
+
+    return @result;
+}
+
+
+sub format_post {
+    my $post = shift;
+    my $result = sprintf "[%10s] %6d (%-12s) | %s",
+                         strftime("%m-%d %H:%M", localtime($post->[4])), # time
+                         $post->[0],    # postId
+                         $post->[2],    # author
+                         $post->[5];    # title
+
+    if ('GBK' eq $terminal_encoding) {
+        return Encode::encode("GBK", $result);
+    }
+
+    return $result;
+}
+
+
+sub daemonize {
+    # http://www.newsmth.net/bbscon.php?bid=226&id=15888
+    defined(my $pid = fork) or die "Can't fork: $!\n";
+    exit if $pid;
+    open STDIN, '/dev/null' or die "Can't read /dev/null: $!\n";
+    open STDOUT, '>/dev/null' or die "Can't write stdout to /dev/null: $!\n";
+    open STDERR, '>/dev/null' or die "Can't write stderr to /dev/null: $!\n";
+    chdir '/' or die "Can't chdir to /: $!\n";
+    umask 0;
+    setsid or die "Can't start a new session: $!\n";
+}
+
+
+sub load_board_list {
+    open (my $fh, 'board.list') || die "Can't read board list: $!\n";
+    while (<$fh>) {
+        chomp;
+        next if /^\s*#/;
+        s/^\s+//;
+        my ($board, $id) = split /\s+/;
+        $lastPostIds{$board} = ($id || 0) if defined $board;
+    }
+    close $fh;
+}
+
+
+sub save_board_list {
+    my $sig = shift;
+    local $SIG{$sig} = 'IGNORE' if defined $sig;
+
+    exit 0 if keys %lastPostIds == 0;
+
+    my @a;
+    push @a, sprintf("%-32s        %s\n", "# board name", "last post id");
+    while (my ($board, $id) = each %lastPostIds) {
+        push @a, sprintf("%-32s        %d\n", $board, $id);
+    }
+    write_file('board.list', {binmode => ':utf8', atomic => 1}, \@a);
+
+    exit 0 if defined $sig;
+}
+
+} # end package main
 
