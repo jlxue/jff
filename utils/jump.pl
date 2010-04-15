8dd6f23df2e6ec0f617aa39aa280a9da3135c949
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-04-15 09:41:02 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@562 1de5394f-e846-0410-8a38-9b14b2994ffe

--- utils/jump.pl
+++ utils/jump.pl
@@ -3,10 +3,11 @@ use strict;
 use warnings;
 
 use Cwd;
+use Fcntl qw/:DEFAULT :flock/;
 use File::Spec;
 use Getopt::Long;
 use Smart::Comments '###';
-use Storable qw/lock_store lock_retrieve/;
+use Storable qw/store_fd fd_retrieve lock_retrieve/;
 use Term::ANSIColor;
 
 use Class::Struct HistoryItem =>
@@ -16,12 +17,17 @@ use Class::Struct HistoryItem =>
     from_dirs     => '%',
     ];
 
-use constant    HISTORY_FILE    => ".jump_history";
+use constant    {
+    HISTORY_FILE    => ".jump_history",
+    MODIFIED        => 0,
+    REMOVED         => 1,
+};
 
 ######################################################################
 ## global variables
 my $g_history_file = File::Spec->catfile($ENV{'HOME'}, HISTORY_FILE);
 my $g_history;              # hash of working directories to HistoryItems
+my @g_history_changed;      # changed entries to $g_history
 my $g_opt_from_work_dir;    # old working directory
 my $g_opt_action = 'search';# prune, dump, add, search
 my %g_action_handler = (
@@ -36,11 +42,15 @@ my %g_action_handler = (
 GetOptions("--from=s"    => \$g_opt_from_work_dir,
            "--action=s"  => \$g_opt_action,
           );
-$g_opt_from_work_dir = getcwd() if !defined $g_opt_from_work_dir;
+if (defined $g_opt_from_work_dir) {
+    $g_opt_from_work_dir = File::Spec->rel2abs(File::Spec->canonpath($g_opt_from_work_dir));
+} else {
+    $g_opt_from_work_dir = getcwd() if !defined $g_opt_from_work_dir;
+}
 
 ######################################################################
 ## load history file
-$g_history = lock_retrieve($g_history_file) if -f $g_history_file;
+$g_history = lock_retrieve($g_history_file) if -s $g_history_file;
 $g_history = {} if !defined $g_history;
 
 ######################################################################
@@ -48,10 +58,8 @@ $g_history = {} if !defined $g_history;
 if (exists $g_action_handler{$g_opt_action}) {
     my $ret = $g_action_handler{$g_opt_action}->(@ARGV);
 
-    if (! $ret and defined $g_history and $g_opt_action ne 'dump') {
-        $ret = lock_store $g_history, $g_history_file;
-
-        $ret = $ret ? 0 : 1;
+    if (! $ret and @g_history_changed > 0) {
+        $ret = safe_store_history();
     }
 
     exit $ret;
@@ -66,7 +74,10 @@ exit 1;
 sub action_add {
     my $to_work_dir = shift;
 
-    return 1 if !defined $to_work_dir or !-d $to_work_dir;
+    return 1 if !defined $to_work_dir or !-d $to_work_dir or !-d $g_opt_from_work_dir;
+    $to_work_dir = File::Spec->rel2abs(File::Spec->canonpath($to_work_dir));
+
+    return 0 if $g_opt_from_work_dir eq $to_work_dir;
 
     my $history_item;
 
@@ -84,12 +95,15 @@ sub action_add {
     my $dirs = $history_item->from_dirs;
     $dirs->{$g_opt_from_work_dir}++;
 
+    $g_history_changed[MODIFIED] = [$to_work_dir, $g_opt_from_work_dir];
+
     return 0;
 }
 
 sub action_search {
     my $pattern = shift;
-    my (@candidates, $choice);
+    my @candidates;
+    my $choice;
 
     if (defined $pattern) {
         @candidates = grep /$pattern/, keys %$g_history;
@@ -100,16 +114,22 @@ sub action_search {
     return 1 if @candidates == 0;
 
     if (@candidates == 1) {
-        $choice = 1;
+        $choice = $candidates[0];
     } else {
-        $choice = choice_directory(@candidates);
+        if (-t STDOUT) {
+            $choice = choice_directory(@candidates);
+            return 1 if !defined $choice;
+        } else {
+            @candidates = sort_candidates(@candidates);
+            for my $c (@candidates) {
+                print $c, "\n";
+            }
+
+            return 0;
+        }
     }
 
-    return 1 if $choice =~ /^q/i;
-
-    --$choice;
-    action_add($candidates[$choice]);
-    print $candidates[$choice], "\n";
+    print $choice, "\n";
 
     return 0;
 }
@@ -126,12 +146,16 @@ sub action_dump {
     return 0;
 }
 
+sub sort_candidates {
+    return sort @_;
+}
+
 sub choice_directory {
-    my @candidates = sort @_;
+    my @candidates = sort_candidates @_;
     my $choice;
 
     do {
-        open my $pipe, "|less -FRX" || die "Can't pipe to \"less\": $!\n";
+        open my $pipe, "|less -FRX" or die "Can't pipe to \"less\": $!\n";
         for (my $i = 0; $i <= $#candidates; ++$i) {
             print $i + 1, "\t", $candidates[$i], "\n";
         }
@@ -141,6 +165,50 @@ sub choice_directory {
         $choice = <STDIN>;
         chomp $choice;
         $choice =~ s/^\s+//;
-    } while ($choice !~ /^q/i && ($choice <= 0 || $choice > @candidates));
+
+        return undef if $choice =~ /^q/i;
+    } while ($choice <= 0 || $choice > @candidates);
+
+    return $candidates[$choice - 1];
+}
+
+sub safe_store_history {
+    sysopen my $fh, $g_history_file, O_RDWR | O_CREAT or die "Can't write to $g_history_file: $!\n";
+    flock $fh, LOCK_EX or die "Can't lock $g_history_file: $!\n";
+
+    my $history = fd_retrieve($fh) if -s $fh;
+    $history = {} if !defined $history;
+
+    my $h = $g_history_changed[MODIFIED];
+    if (defined $h) {
+        my ($to, $from) = @$h;
+        my $ondisk_item = $history->{$to};
+        my $item = $g_history->{$to};
+
+        if (defined $ondisk_item) {
+            $ondisk_item->access_time($item->access_time) if
+                    $ondisk_item->access_time < $item->access_time;
+            $ondisk_item->hit_count($ondisk_item->hit_count + 1);
+
+            my $dirs = $ondisk_item->from_dirs;
+            $dirs->{$from}++;
+        } else {
+            $history->{$to} = $g_history->{$to};
+        }
+    }
+
+    $h = $g_history_changed[REMOVED];
+    if (defined $h) {
+    }
+
+    sysseek($fh, 0, 0) or die "Can't seek in $g_history_file: $!\n";
+    truncate($fh, 0) or die "Can't truncate $g_history_file: $!\n";
+    my $ret = store_fd($history, $fh);
+    $ret = $ret ? 0 : 1;
+
+    flock $fh, LOCK_UN;
+    close $fh;
+
+    return $ret;
 }
 
---
+++ utils/jump.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+__JUMP_PL=jump.pl
+
+j () {
+    declare -a candidates
+    declare -i i=0
+    local from
+
+    [ "$1" = "-" -o -d "$1" ] && {
+        from=$PWD
+        cd "$1" && "$__JUMP_PL" -a add -f $from $PWD
+        return
+    }
+
+    while read && [ "$REPLY" ]; do
+        candidates[$((i++))]=$REPLY
+    done < <("$__JUMP_PL" -a search "$1")
+
+    [ ${#candidates[@]} -eq 0 ] && {
+        echo "No candidate!" >&2
+        return
+    }
+
+    [ ${#candidates[@]} -eq 1 ] && {
+        from=$PWD
+        cd ${candidates[0]} && "$__JUMP_PL" -a add -f $from $PWD
+        return
+    }
+
+    while :; do
+        for ((i=0; i < ${#candidates[@]}; ++i)); do
+            printf "%4s  %s\n" $((i+1)) ${candidates[$i]}
+        done | less -FRX
+
+        printf "Which directory? [0 to quit, 1 ~ ${#candidates[@]} to select, or view again] "
+        read
+
+        # 0 to quit
+        [ "0" = "$REPLY" ] && return
+
+        # invalid number
+        [ 0 -eq $(expr "$REPLY" : "[0-9]\+$") ] && continue
+
+        i=$REPLY
+        [ 0 -eq $((i <= 0 || i > ${#candidates[@]})) ] && {
+            from=$PWD
+            cd ${candidates[$((i-1))]} && "$__JUMP_PL" -a add -f $from $PWD
+            return
+        }
+    done
+}
+
+unset __JUMP_PL
+
