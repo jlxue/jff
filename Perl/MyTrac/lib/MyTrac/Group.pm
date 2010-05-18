2a08ad756e698542dde05f43e54b37ccfd384922
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-05-18 10:36:50 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@593 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/MyTrac/lib/MyTrac/Group.pm
+++ Perl/MyTrac/lib/MyTrac/Group.pm
@@ -7,7 +7,8 @@ our $VERSION = '0.01';
 extends 'MyTrac::Item';
 
 has 'name'      => (is => 'rw', isa => 'Str');
-has 'memebers'  => (is => 'rw', isa => 'ArrayRef[Str]');
+# use id instead of user name, because name can be changed!
+has 'memeberIds'=> (is => 'rw', isa => 'ArrayRef[Str]');
 
 no Any::Moose;
 __PACKAGE__->meta->make_immutable();
--- Perl/MyTrac/lib/MyTrac/User.pm
+++ Perl/MyTrac/lib/MyTrac/User.pm
@@ -7,8 +7,8 @@ our $VERSION = '0.01';
 extends 'MyTrac::Item';
 
 has 'name'      => (is => 'rw', isa => 'Str');
-has 'email'     => (is => 'rw', isa => 'Str');
-has 'groups'    => (is => 'rw', isa => 'ArrayRef[Str]');
+# use id instead of group name, because group name can be changed!
+has 'groupIds'  => (is => 'rw', isa => 'ArrayRef[Str]');
 
 no Any::Moose;
 __PACKAGE__->meta->make_immutable();
