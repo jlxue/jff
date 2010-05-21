8d61e199b6cce711199051badf939bf9bf8675c4
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-05-21 10:34:13 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@611 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/MyTrac/lib/MyTrac/Database.pm
+++ Perl/MyTrac/lib/MyTrac/Database.pm
@@ -1,7 +1,7 @@
 package MyTrac::Database;
 use Any::Moose;
 use Digest::SHA1 qw/sha1_hex/;
-use Fcntl qw/:flock/;
+use Fcntl qw/:DEFAULT :flock/;
 use File::Spec;
 use File::Temp;
 use Guard;      # or Scope::Guard?
@@ -26,13 +26,20 @@ sub BUILD {
 
     $self->json(JSON->new->utf8->canonical->pretty->relaxed);
 
-    if (! -e File::Spec->catdir($self->git_dir)) {
+    if (! -e $self->git_dir) {
         my @dirs = File::Spec->splitdir($self->git_dir);
         pop @dirs;
         my $repos = File::Spec->catdir(@dirs);
 
         confess "Can't initialize database!" if
                 0 != system(qw/git init -q/, $repos);
+    }
+
+    my $lockfile = $self->git_lockfile_path;
+    if (! -e $lockfile) {
+        sysopen my $fh, $lockfile, O_WRONLY | O_CREAT, 0644;
+        confess "Can't create lock file $lockfile: $!" if ! defined $fh;
+        close $fh;
     }
 }
 
@@ -89,6 +96,7 @@ sub from_json {
 
 sub transact($\%&@) {
     my ($self, $options, $func, @args) = @_;
+    my $lock_fh;
 
     confess "Transaction can't be nested!" if defined $self->operations;
     $self->operations([]);
@@ -102,6 +110,8 @@ sub transact($\%&@) {
     scope_guard {
         # Destructors of database operations will do cleanup.
         $self->operations(undef);
+
+        close $lock_fh if defined $lock_fh;
     };
 
     my $result = $func->(@args);
@@ -122,10 +132,43 @@ sub transact($\%&@) {
         }
     }
 
-    # commit this transaction
-    for my $op (@$operations) {
+    sysopen $lock_fh, $self->git_lockfile_path, O_WRONLY;
+    confess "Can't open lock file for transaction: $!" if !defined $lock_fh;
+    flock($lock_fh, LOCK_EX) or confess "Can't lock for transaction: $!";
 
+    # commit this transaction
+    my @insert_files = ();
+    my @update_files = ();
+    my @delete_files = ();
+    for my $op (@$operations) {
+        if ($op->isa('MyTrac::Database::InsertOp')) {
+            push @insert_files, $op->filename;
+        } elsif ($op->isa('MyTrac::Database::UpdateOp')) {
+            push @update_files, $op->filename;
+        } elsif ($op->isa('MyTrac::Database::DeleteOp')) {
+            push @delete_files, $op->filename;
+        } else {
+            Carp::cluck("This kind operation shouldn't be added into transaction!");
+        }
     }
+    my @files = (@insert_files, @update_files);
+    if (@files > 0) {
+        confess "Failed to git-add files: @files" if
+                0 != system($self->git_cmd('add', @files));
+    }
+    if (@delete_files > 0) {
+        confess "Failed to git-rm files: @delete_files" if
+                0 != system($self->git_cmd(qw/rm -f --cached --/, @delete_files));
+    }
+
+    push @files, @delete_files;
+    my @commit_options = ();
+    push @commit_options, '--author=' . $options->{author} if exists $options->{author};
+    push @commit_options, '-m', $options->{shortlog} if exists $options->{shortlog};
+    push @commit_options, '-m', $options->{log} if exists $options->{log};
+    push @commit_options, '--';
+    confess "Failed to git-commit files: @files" if
+            0 != system($self->git_cmd(qw/commit -q/, @commit_options, @files));
 
     # they are successful
     for my $op (@$operations) {
@@ -234,6 +277,10 @@ sub git_path {
     my ($self, $filename) = @_;
 
     return File::Spec->catfile($self->work_tree, $filename);
+}
+
+sub git_lockfile_path {
+    return File::Spec->catfile($_[0]->git_dir, 'mytrac.lck');
 }
 
 no Any::Moose;
