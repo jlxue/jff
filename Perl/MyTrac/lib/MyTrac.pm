4b435c23bd5e6b888a1f8565e7bc5139a3a07657
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-05-27 13:12:38 +0000

    add ckedit 3.2.1
    git-svn-id: https://jff.googlecode.com/svn/trunk@631 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/MyTrac/lib/MyTrac.pm
+++ Perl/MyTrac/lib/MyTrac.pm
@@ -1,45 +1,103 @@
 package MyTrac;
 
-use warnings;
-use strict;
 use base 'Titanium';
 use Carp;
+use MyTrac::Article;
+use MyTrac::Database;
+use strict;
+use warnings;
+use constant MODE_PARAM => 'm';
 
 our $VERSION = '0.01';
 
 sub setup {
-    my ($self) = @_;
+    my ($c) = @_;
+    my $db = new MyTrac::Database({
+            git_dir     => $c->param('git_dir'),
+            work_tree   => $c->param('work_tree'),
+        });
+    $c->{__MYTRAC_DB} = $db;
 
-    $self->start_mode('runmode1');
-    $self->error_mode('runmode1');
-    $self->run_modes( [qw/ runmode1 /] );
-    if ( !$self->tmpl_path ) {
-        ( my $tp = 'MyTrac' ) =~ s{-}{/}gmsx;
-        $tp .= '/templates';
-        foreach my $inc (@INC) {
-            if ( -d "$inc/$tp" ) {
-                $self->tmpl_path("$inc/$tp");
-                last;
-            }
-        }
+    $c->mode_param(MODE_PARAM);
+    $c->start_mode('list');
+    $c->run_modes([qw/list insert update delete select/]);
+}
+
+sub list {
+    my ($c) = @_;
+    my ($db, $q, $s) = ($c->db, $c->query, $c->session);
+
+    if ($q->request_method ne 'GET') {
+        return;
     }
-    $self->run_modes( AUTOLOAD => 'runmode1' );
-    return;
 }
 
-sub runmode1 {
-    my ($self) = @_;
+sub insert {
+    my ($c) = @_;
+    my ($db, $q, $s) = ($c->db, $c->query, $c->session);
 
-    my $template = $self->load_tmpl;
-    $template->param( message => 'Hello world!' );
-    return $template->output;
+    if ($q->request_method ne 'POST') {
+        return;
+    }
+
+    my $title = $q->param('title');
+    my $content = $q->param('content');
+    my $tags = $q->param('tags');
+
+    if (!defined $title or !defined $content) {
+        return;
+    }
+
+    $title =~ s/^\s+|\s+$//g;
+    if (length($title) == 0) {
+        return;
+    }
+
+    my $article = new MyTrac::Article({title => $title, content => $content});
+    if (defined $tags) {
+        $tags =~ s/^\s+|\s+$//g;
+        $tags =~ s/\s*,+\s*/,/g;
+        $tags =~ s/^,+|,+$//g;
+
+        $article->tags($tags) if length($tags) > 0;
+    }
+
+    eval {
+    }
 }
 
+sub update {
+    my ($c) = @_;
+    my ($db, $q, $s) = ($c->db, $c->query, $c->session);
 
-sub function1 {
-    my ($self) = @_;
+    if ($q->request_method ne 'POST') {
+        return;
+    }
+}
 
-    return 1;
+sub delete {
+    my ($c) = @_;
+    my ($db, $q, $s) = ($c->db, $c->query, $c->session);
+
+    if ($q->request_method ne 'POST') {
+        return;
+    }
+}
+
+sub select {
+    my ($c) = @_;
+    my ($db, $q, $s) = ($c->db, $c->query, $c->session);
+    my @ids;
+
+    if ($q->request_method ne 'GET') {
+        return;
+    }
+}
+
+sub db {
+    my ($c) = @_;
+
+    return $c->{__MYTRAC_DB};
 }
 
 1;
--- Perl/MyTrac/server.pl
+++ Perl/MyTrac/server.pl
@@ -5,12 +5,16 @@ use lib 'lib';
 use MyTrac;
 
 my $app = MyTrac->new(PARAMS => {
-
-});
-
+        "git_dir"   => "db/.git",
+        "work_tree" => "db",
+    });
 my $server = CGI::Application::Server->new();
+
 $server->document_root('./t/www');
+
 $server->entry_points({
-    '/article'       => $app,
+    '/mytrac'       => $app,
 });
+
 $server->run;
+
