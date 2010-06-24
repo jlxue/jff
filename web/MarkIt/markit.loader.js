bdd110bc566152ef325d595d6ad3d012ebc7a17a
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-06-24 06:03:26 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@669 1de5394f-e846-0410-8a38-9b14b2994ffe

--- web/MarkIt/markit.loader.js
+++ web/MarkIt/markit.loader.js
@@ -1,15 +1,22 @@
 (function() {
-    var dialog = document.getElementById('markit-dialog');
+    var scripts = document.getElementsByTagName('script');
+    var url='##MARKIT_URL##';
 
-    if (! dialog) {
-        var s = document.createElement('script');
-        s.setAttribute('src', '##MARKIT_URL##');
-        document.getElementsByTagName('body')[0].appendChild(s);
-    } else if (dialog.getAttribute('ready')) {
-        $(dialog).toggle();
-    } else {
-        alert("Loading MarkIt dialog, please try again later.");
+    for (var i = 0; i < scripts.length; ++i) {
+        var src = scripts[i].getAttribute('src');
+        if (src === url) {
+            var dialog = document.getElementById('markit-dialog');
+            if (dialog) {
+                $(dialog).toggle();
+            } else {
+                alert('Loading MarkIt dialog, please try again later.');
+            }
+            return;
+        }
     }
 
+    var s = document.createElement('script');
+    s.setAttribute('src', url);
+    document.getElementsByTagName('body')[0].appendChild(s);
 })()
 
