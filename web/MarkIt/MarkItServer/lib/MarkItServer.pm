66dfa1c14dcfa7c03adc85f83d73d0ccb01bd246
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-06-28 10:34:05 +0000

    server skeleton generated by dancer script
    git-svn-id: https://jff.googlecode.com/svn/trunk@728 1de5394f-e846-0410-8a38-9b14b2994ffe

---
+++ web/MarkIt/MarkItServer/lib/MarkItServer.pm
@@ -0,0 +1,10 @@
+package MarkItServer;
+use Dancer ':syntax';
+
+our $VERSION = '0.1';
+
+get '/' => sub {
+    template 'index';
+};
+
+true;
---
+++ web/MarkIt/MarkItServer/public/dispatch.cgi
@@ -0,0 +1,3 @@
+#!/usr/bin/perl
+use Plack::Runner;
+Plack::Runner->run('/home/dieken/home/jff/web/MarkIt/MarkItServer/app.psgi');
