eb51fd7594972ecd18163ef95692a99df85a0555
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-06-28 10:36:50 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@733 1de5394f-e846-0410-8a38-9b14b2994ffe

--- web/MarkIt/MarkItServer/lib/MarkItServer.pm
+++ web/MarkIt/MarkItServer/lib/MarkItServer.pm
@@ -1,156 +1,31 @@
-
-=head1 NAME
-
-MarkItServer - The great new MarkItServer web application!
-
-=head1 SYNOPSIS
-
-    use MarkItServer;
-    my $app = MarkItServer->new();
-    $app->run();
-
-=head1 ABSTRACT
-
-A brief summary of what MarkItServer does.
-
-=cut
-
 package MarkItServer;
 
 use warnings;
 use strict;
 use base 'Titanium';
 use Carp qw( croak );
-
-=head1 VERSION
-
-This document describes MarkItServer Version 0.01
-
-=cut
+use Log::Dispatch::File;
 
 our $VERSION = '0.01';
 
-=head1 DESCRIPTION
-
-Overview of functionality and purpose of
-web application module MarkItServer...
-
-=head1 METHODS
-
-=head2 SUBCLASSED METHODS
-
-=head3 setup
-
-Sets up the run mode dispatch table and the start, error, and default run modes.
-If the template path is not set, sets it to a default value.
-
-TODO: change all these values to ones more appropriate for your application.
-
-=cut
-
 sub setup {
-    my ($self) = @_;
+    my ($c) = @_;
 
-    $self->start_mode('runmode1');
-    $self->error_mode('runmode1');
-    $self->run_modes( [qw/ runmode1 /] );
-    if ( !$self->tmpl_path ) {
-        ( my $tp = 'MarkItServer' ) =~ s{-}{/}gmsx;
-        $tp .= '/templates';
-        foreach my $inc (@INC) {
-            if ( -d "$inc/$tp" ) {
-                $self->tmpl_path("$inc/$tp");
-                last;
-            }
-        }
-    }
-    $self->run_modes( AUTOLOAD => 'runmode1' );
+    $c->start_mode('start');
+    $c->run_modes([qw/start/]);
     return;
 }
 
-=pod
+sub start {
+    my ($c) = @_;
+    #my $file = $c->query->path_info();
+    #$c->log->info("got $file");
+    my $file = './t/www/markit.min.js';
 
-TODO: Other methods inherited from Titanium go here.
-
-=head2 RUN MODES
-
-=head3 runmode1
-
-  * Purpose
-  * Expected parameters
-  * Function on success
-  * Function on failure
-
-TODO: Describe runmode1 here. Rename runmode1 to something more appropriate 
-for your application.
-
-=cut
-
-sub runmode1 {
-    my ($self) = @_;
-
-    my $template = $self->load_tmpl;
-    $template->param( message => 'Hello world!' );
-    return $template->output;
+    $c->header_add('-Access_Control_Allow_Origin' => '*',
+                   '-type'  => 'application/javascript');
+    $c->stream_file($file);
 }
 
-=head2 OTHER METHODS
+1;
 
-=head3 function1
-
-TODO: Describe function1 here.  Rename function1 to something more appropriate
-for your application.
-
-=cut
-
-sub function1 {
-    my ($self) = @_;
-
-    return 1;
-}
-
-=pod
-
-TODO: Other methods in your public interface go here.
-
-=cut
-
-# TODO: Private methods go here. Start their names with an _ so they are skipped
-# by Pod::Coverage.
-
-=head1 BUGS AND LIMITATIONS
-
-There are no known problems with this module.
-
-Please report any bugs or feature requests to
-C<bug-markitserver at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=MarkItServer>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
-
-=head1 SEE ALSO
-
-L<Titanium>
-
-=head1 THANKS
-
-List acknowledgements here or delete this section.
-
-=head1 AUTHOR
-
-Liu Yubao, C<< <Yubao.Liu at gmail.com> >>
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2010 Liu Yubao, all rights reserved.
-
-This program is released under the following license: GPL v3
-
-The full text of the license can be found in the LICENSE file included
-with this distribution.
-
-=cut
-
-1;    # End of MarkItServer
-
-__END__
--- web/MarkIt/MarkItServer/server.pl
+++ web/MarkIt/MarkItServer/server.pl
@@ -1,16 +1,15 @@
+#!/usr/bin/perl
 use warnings;
 use strict;
 use CGI::Application::Server;
 use lib 'lib';
 use MarkItServer;
 
-my $app = MarkItServer->new(PARAMS => {
-
-});
-
 my $server = CGI::Application::Server->new();
 $server->document_root('./t/www');
 $server->entry_points({
-    '/index.cgi' => $app,
+    '/markit.min.js'         => MarkItServer->new(PARAMS => {}),
 });
+
 $server->run;
+
