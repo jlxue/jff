b49b0b5151942dc28883da63a3bc4cc136ef1dca
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-06-29 04:35:21 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@737 1de5394f-e846-0410-8a38-9b14b2994ffe

--- web/MarkIt/server/lib/MarkIt/StaticFile.pm
+++ web/MarkIt/server/lib/MarkIt/StaticFile.pm
@@ -1,6 +1,7 @@
 package MarkIt::StaticFile;
-
+use Cwd qw/abs_path/;
 use base 'MarkIt::Base';
+use constant ROOT   => abs_path('t/www');
 
 our $VERSION = '0.01';
 
@@ -16,14 +17,25 @@ sub view {
 
     my $path = $c->param('path');
 
-    $c->log->info("got [$path]\n");
-    my $file = './t/www/markit.min.js';
+    $c->log->info("request static file: [$path]\n");
+    if (! defined($path) || $path eq '/' || length($path) == 0) {
+        $path = ROOT . '/index.html';
+    } else {
+        my $p= abs_path(ROOT . '/' . $path);
+        if (index($p, ROOT) != 0) {
+            $c->error(title => "Stop!", msg => "Can't access " . $p);
+            return;
+        }
 
-    if ($file =~ /\.js$/) {
+        $path = $p;
+    }
+
+    $c->log->info("response static file: [$path]\n");
+    if ($path =~ /\.js$/) {
         $c->header_add('-type'  => 'application/javascript');
     }
 
-    $c->stream_file($file);
+    $c->stream_file($path);
 }
 
 1;
