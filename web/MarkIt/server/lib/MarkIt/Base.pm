fd20b575bc481c9bbc48bdb20e56eb84f00da0df
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-07-02 16:03:29 +0000

    process cross domain XMLHTTPRequest with preflighted request
    See https://developer.mozilla.org/en/http_access_control
    
    git-svn-id: https://jff.googlecode.com/svn/trunk@759 1de5394f-e846-0410-8a38-9b14b2994ffe

--- web/MarkIt/server/lib/MarkIt/Base.pm
+++ web/MarkIt/server/lib/MarkIt/Base.pm
@@ -16,18 +16,58 @@ sub cgiapp_init {
     my $password = "";
     my %attr = ();
     $c->dbh_config($data_source, $username, $password, \%attr);
+
+    $c->run_modes([qw/preflight/]);
 }
+
 
 sub cgiapp_prerun {
     my ($c) = @_;
 
     $c->header_add(-charset => "UTF-8");
+
+    my $q = $c->query;
+    my $hasAccessControl = 0;
+
+    $c->log->info("cgiapp_prerun: request_method=" . $q->request_method . "\n");
+
+    # we'd better use $q->http("Access-Control-Request-Method").
+    if (exists $ENV{"HTTP_ACCESS_CONTROL_REQUEST_METHOD"}) {
+        $c->header_add(-Access_Control_Request_Methods =>
+                "POST, GET, PUT, HEAD, DELETE, OPTIONS");
+        $hasAccessControl = 1;
+    }
+
+    if (exists $ENV{"HTTP_ACCESS_CONTROL_REQUEST_HEADERS"}) {
+        $c->header_add(-Access_Control_Request_Headers =>
+                $ENV{"HTTP_ACCESS_CONTROL_REQUEST_HEADERS"});
+        $hasAccessControl = 1;
+    }
+
+    if (exists $ENV{"HTTP_ORIGIN"}) {
+        $c->header_add('-Access_Control_Allow_Origin' => '*');
+        $hasAccessControl = 1;
+    }
+
+    if ($hasAccessControl) {
+        $c->header_add(-Access_Control_Max_Age => 172800);
+    }
+
+    if ($q->request_method eq 'OPTIONS') {
+        $c->prerun_mode("preflight");
+    }
 }
 
-sub cgiapp_postrun {
-    my ($c, $output_ref) = @_;
+#sub cgiapp_postrun {
+#    my ($c, $output_ref) = @_;
+#}
 
-    $c->header_add('-Access_Control_Allow_Origin' => '*');
+sub preflight {
+    my ($c) = @_;
+
+    $c->log->info("preflighted!\n");
+
+    return "";
 }
 
 1;
--- web/MarkIt/server/server.pl
+++ web/MarkIt/server/server.pl
@@ -5,6 +5,12 @@ use CGI::Application::Server;
 use lib 'lib';
 use MarkIt;
 
+*{CGI::Application::Server::valid_http_method} = sub {
+    my $self   = shift;
+    my $method = shift or return 0;
+    return $method =~ /^(?:GET|POST|OPTIONS|HEAD|PUT|DELETE)$/;
+};
+
 my $server = CGI::Application::Server->new();
 $server->document_root('./t/www');
 $server->entry_points({
