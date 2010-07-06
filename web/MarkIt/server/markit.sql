c24273d0c5e2543da6e0557e8a431ff3aa0e68e8
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-07-06 10:28:41 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@782 1de5394f-e846-0410-8a38-9b14b2994ffe

--- web/MarkIt/server/markit.sql
+++ web/MarkIt/server/markit.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS marks
 (
-    left    integer DEFAULT 0 CHECK (left >= 0),
-    top     integer DEFAULT 0 CHECK (top >= 0),
     key     string NOT NULL,
     url     string NOT NULL,
     title   string DEFAULT "",
     marks   string DEFAULT "",
+    targs   string DEFAULT "",
+    left    integer DEFAULT 0 CHECK (left >= 0),
+    top     integer DEFAULT 0 CHECK (top >= 0),
 
     PRIMARY KEY (key, url)
 );
