cb7391d47015209970461c270872728ae14547b0
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-08-13 18:16:04 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@819 1de5394f-e846-0410-8a38-9b14b2994ffe

--- XDE/user-xde.sh
+++ XDE/user-xde.sh
@@ -7,27 +7,49 @@ set -e
 
 XDE_HOME="`dirname $0`"
 
+BACKUP_DIR="$HOME/.dot_files"
+TIMESTAMP=`date +%Y%m%d-%H%M%S`
 
 ###########################################################
 ## functions
 
+backup () {
+    local f="$1"
+    local dest="$BACKUP_DIR/${f#$HOME/}_$TIMESTAMP~"
+
+    [ -e "$f" ] && {
+        mkdir -p "$(dirname "$dest")"
+        cp "$f" "$dest"
+    }
+}
+
 sync_dot_file () {
-    local f="$1" dest="$2"
+    local f="$1" dest="$2" backup_dir
 
     [ -z "$dest" ] && dest="$HOME/${f#$XDE_HOME/xde/dot}"
+    backup_dir="$BACKUP_DIR/${dest#$HOME/}"
 
     if [ -d "$f" ]; then
+        mkdir -p "$backup_dir"
+
         echo "rsync directory : $f to $dest ..."
-        rsync -ar $f/ $dest
+        rsync -ar -b --suffix=_$TIMESTAMP~ --backup-dir="$backup_dir" $f/ $dest
     else
+        backup_dir="$(dirname "$backup_dir")"
+        mkdir -p "$backup_dir"
+
         echo "rsync file      : $f to $dest ..."
-        rsync -a $f $dest
+        rsync -a -b --suffix=_$TIMESTAMP~ --backup-dir="$backup_dir" $f $dest
     fi
 }
 
+###########################################################
+## initialization
+mkdir -p "$BACKUP_DIR"
 
 ###########################################################
 ## set theme
+backup $HOME/.gtkrc-2.0
 gtk-theme-switch2 /usr/share/themes/Clearlooks
 
 
