f0339b79ae32cd2b20d62996c68fa1f332b42321
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-08-14 11:19:12 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@828 1de5394f-e846-0410-8a38-9b14b2994ffe

---
+++ XDE/release.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+RELEASE=XDE-`date +%Y%m%d`
+
+echo "Package files to $RELEASE.tar.gz ..."
+
+tar -czf $RELEASE.tar.gz --transform "s|^|$RELEASE/|" \
+    install-xde.sh user-xde.sh manual.txt xde
+
+echo "Done."
+
