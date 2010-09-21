96f9131293344151760ada59f4d5a57ed3fb3a74
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-09-21 14:16:02 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@862 1de5394f-e846-0410-8a38-9b14b2994ffe

--- sysadmin/install.sh
+++ sysadmin/install.sh
@@ -4,12 +4,31 @@ set -e
 
 PACKAGES="isc-dhcp-server-ldap \
  pdns-backend-ldap \
- slapd ldap-utils libnss-ldapd \
+ gnutls-bin \
+ slapd ldap-utils libnss-ldapd libsasl2-modules-gssapi-mit \
  krb5-admin-server krb5-kdc-ldap libpam-krb5"
 
 aptitude install $PACKAGES
 
-# Kerberos:
+#### OpenLDAP:
+
+certtool --generate-privkey --outfile /etc/ldap/self-ca-key.pem
+certtool --generate-self-signed --load-privkey /etc/ldap/self-ca-key.pem --outfile /etc/ldap/self-ca-cert.pem
+# CN must be fully qualified domain name, see http://www.openldap.org/doc/admin24/tls.html 16.1.1 Server Certificates
+
+chown openldap:openldap /etc/ldap/self-ca-key.pem
+chmod 0600 /etc/ldap/self-ca-key.pem
+chmod 0644 /etc/ldap/self-ca.pem
+
+
+ldapmodify -c -f ldap-mods -Y EXTERNAL -H ldapi:///
+
+/etc/init.d/slapd stop
+slapindex
+chown -R openldap:openldap /var/lib/ldap
+/etc/init.d/slapd start
+
+#### Kerberos:
 #   Default kerberos version 5 realm?       EXAMPLE.COM
 #   Kerberos servers for your realm:        krb.example.com
 #   Administrative server for your Kerberos realm:      krb.example.com
@@ -24,3 +43,9 @@ if [ ! -e /var/lib/krb5kdc/principal ]; then
      echo "add_policy -minlength 8 -minclasses 2 user";) | kadmin.local
 fi
 
+
+# Backup:
+#   /etc        (no krb5 keytab and stash!)
+#   /var/backups
+#   /var/lib
+#   /root
