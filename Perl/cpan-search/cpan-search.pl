4d2fa91050cf9b88c5b0f041d336aeebd64ee7be
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-09-27 17:00:27 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@871 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/cpan-search/cpan-search.pl
+++ Perl/cpan-search/cpan-search.pl
@@ -1,30 +1,12 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Data::Dumper;
 use File::Basename;
+use IO::Uncompress::AnyUncompress qw(anyuncompress $AnyUncompressError);
 use LWP::UserAgent;
 
-my @MIRROR_FILES = qw(
-    authors/00whois.html
-    authors/00whois.xml
-    authors/01mailrc.txt.gz
-    indices/du-k.gz
-    indices/find-ls.gz
-    indices/ls-lR.gz
-    modules/00modlist.long.html
-    modules/01modules.index.html
-    modules/01modules.mtime.html
-    modules/01modules.mtime.rss
-    modules/02packages.details.txt.gz
-    modules/03modlist.data.gz
-);
-
-my $MIRROR_SITE = 'http://cpan.wenzk.com/';
-
-mirror_files($MIRROR_SITE, @MIRROR_FILES);
-
-
-sub mirror_files {
+sub mirror_files($@) {
     my ($site, @files) = @_;
     my $i = 1;
     my $count = @files;
@@ -48,4 +30,53 @@ sub mirror_files {
         die "Failed to mirror $remote_file: " . $response->status_line if $response->is_error;
     }
 }
+
+sub filter_find_ls($) {
+    my $z = IO::Uncompress::AnyUncompress->new($_[0]) or die "anyuncompress failed: $AnyUncompressError\n";
+    my (@meta_files, %author_id_map, @dists);
+
+    while (<$z>) {
+        next if ! /\d authors\//;
+
+        my @a = split;
+        if ($a[2] eq "l") {
+            if ($a[8] !~ m|^authors/id/|) {
+                $author_id_map{ substr($a[8], 8) } = substr($a[9], 3);
+            }
+        } elsif ($a[2] =~ /^-/) {
+            if ($a[8] =~ /\.meta$/) {
+                push @meta_files, [ @a[6..8] ];
+            } elsif ($a[8] =~ /[\.\-_](?:gz|bz2|zip|tgz|tbz2|tar|Z|rar)$/i) {
+                push @dists, [ @a[6..8] ];
+            }
+        }
+    }
+
+    $z->close;
+
+    return (\@meta_files, \%author_id_map, \@dists);
+}
+
+######## MAIN ENTRY ##################################
+my @MIRROR_FILES = qw(
+    authors/00whois.html
+    authors/00whois.xml
+    authors/01mailrc.txt.gz
+    indices/du-k.gz
+    indices/find-ls.gz
+    indices/ls-lR.gz
+    modules/00modlist.long.html
+    modules/01modules.index.html
+    modules/01modules.mtime.html
+    modules/01modules.mtime.rss
+    modules/02packages.details.txt.gz
+    modules/03modlist.data.gz
+);
+
+my $MIRROR_SITE = 'http://cpan.wenzk.com/';
+
+#mirror_files($MIRROR_SITE, @MIRROR_FILES);
+
+my ($meta_files, $author_id_map, $dists) = filter_find_ls($ARGV[0]);
+print Dumper($author_id_map);
 
