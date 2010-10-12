4309a1eb977ff8001da75b42f341d63cdc473d5d
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-10-12 15:44:25 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@882 1de5394f-e846-0410-8a38-9b14b2994ffe

--- XDE/xde/dot.gnus.el
+++ XDE/xde/dot.gnus.el
@@ -1,5 +1,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; 用户名字和邮件地址
+;;; 用户缺省名字和邮件地址
 
 (setq user-full-name "Yubao Liu")
 (setq user-mail-address "Yubao.Liu@gmail.com")
@@ -11,19 +11,25 @@
       '(nnimap "gmail"
                (nnimap-address "imap.googlemail.com")
                (nnimap-server-port 993)
-               (nnimap-stream ssl)))
-;(setq gnus-secondary-select-methods
-;      '((....)))
+               (nnimap-stream ssl)     ; ssl requires openssl, tls requires gnutls-cli
+               ))
+
+(setq gnus-secondary-select-methods
+      '(
+        (nnimap "yahoo-corp"
+                (nnimap-address "kr1-webmail.corp.yahoo.com")
+                (nnimap-server-port 993)
+                (nnimap-stream ssl))
+        ))
 
 ;; 自动登录
 ;; 在 ~/.authinfo 写入此行，替换 USERNAME 和 PASSWORD:
 ;; machine imap.googlemail.com login USERNAME@gmail.com password PASSWORD port 993
 
 ;; 允许 [Gmail]/... 这样的邮箱名字
-(setq gnus-invalid-group-regexp "...")
-(setq gnus-ignored-newsgroup "...")
+;(setq gnus-ignored-newsgroup "...")
 
-;; 下载邮件到本地
+;; 下载邮件到本地再阅读
 ;(setq mail-source-primary-source)
 ;(setq mail-sources)
 
@@ -51,48 +57,67 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 发信的配置
-(setq message-send-mail-function 'smtpmail-send-it
+
+(setq message-send-mail-function 'message-smtpmail-send-it
       smtpmail-starttls-credentials '(("smtp.googlemail.com" 465 nil nil))
       smtpmail-auth-credentials '(("smtp.googlemail.com" 465 "Yubao.Liu@gmail.com" nil))
       smtpmail-default-smtp-server "smtp.googlemail.com"
       smtpmail-smtp-server "smtp.googlemail.com"
       smtpmail-smtp-service 465)
 
+(setq gnus-posting-styles
+      '(
+        ("*"
+         (name "Yubao Liu")
+         (address "Yubao.Liu@gmail.com")
+         (sigature (lamabda () (concat "Hello world:\n\n" (shell-command-to-string "fortune"))))
+         (eval (setq smtpmail-starttls-credentials '(("smtp.googleemail.com" 465 nil nil))
+                     smtpmail-auth-credentials '(("smtp.googlemail.com" 465 "Yubao.Liu@gmail.com" nil))
+                     smtpmail-default-smtp-server "smtp.googleemail.com"
+                     smtp-smtp-service 465)))
 
-;(setq gnus-posting-styles
-;      '((...)))
+        ("nnmaildir:126"
+         (name "dieken")
+         (address "dieken@126.com")
+         (eval (setq message-sendmail-extra-arguments '("-a" "gmail"))))
+
+        ("nnimap:yahoo-corp"
+         (address "liuyb@yahoo-inc.com")
+         (eval (setq smtpmail-auth-credentials '(("smarthost.yahoo.com" 25 "liuyb" nil))
+                     smtp-smtp-service 25)))
+        ))
 
 ;; 不同邮箱用各自的 SMTP 服务器
-(defun fs-change-smtp ()
-  "Change the SMTP server according to the current from line."
-  (save-excursion
-    (let ((from
-           (save-restriction
-             (message-narrow-to-headers)
-             (message-fetch-field "from"))))
-      (message "From is `%s', setting `smtpmail-smtp-server' to `%s'"
-               from
-               (cond
-                ((string-match YouAccount@gmail.com from)
-                 ;; Use stmp-auth
-                 (message "Using smtp-auth")
-                 ;; Sending mail
-                 (setq message-send-mail-function 'smtpmail-send-it)
-                 (setq smtpmail-starttls-credentials '(("127.0.0.1" 466 nil nil)))
-                 (setq smtpmail-auth-credentials '(("127.0.0.1" 466 "YouAccount@gmail.com" nil)))
-                 (setq smtpmail-default-smtp-server "127.0.0.1")
-                 (setq smtpmail-smtp-server "127.0.0.1")
-                 (setq smtpmail-smtp-service 466)
-                 )
-                ;;((string-match "YouAccount@gmail.com" from)
-                ;; Use local sendmail
-                ;; (message "Using local sendmail")
-                ;;(setq message-send-mail-function `message-send-mail-with-sendmail))
-                (t
-                 (error
-                  (concat "Don't know which mail server to use for "
-                          from))))))))
-(add-hook 'message-setup-hook 'fs-change-smtp)
+;(defun fs-change-smtp ()
+;  "Change the SMTP server according to the current from line."
+;  (save-excursion
+;    (let ((from
+;           (save-restriction
+;             (message-narrow-to-headers)
+;             (message-fetch-field "from"))))
+;      (message "From is `%s', setting `smtpmail-smtp-server' to `%s'"
+;               from
+;               (cond
+;                ((string-match YouAccount@gmail.com from)
+;                 ;; Use stmp-auth
+;                 (message "Using smtp-auth")
+;                 ;; Sending mail
+;                 (setq message-send-mail-function 'smtpmail-send-it)
+;                 (setq smtpmail-starttls-credentials '(("127.0.0.1" 466 nil nil)))
+;                 (setq smtpmail-auth-credentials '(("127.0.0.1" 466 "YouAccount@gmail.com" nil)))
+;                 (setq smtpmail-default-smtp-server "127.0.0.1")
+;                 (setq smtpmail-smtp-server "127.0.0.1")
+;                 (setq smtpmail-smtp-service 466)
+;                 )
+;                ;;((string-match "YouAccount@gmail.com" from)
+;                ;; Use local sendmail
+;                ;; (message "Using local sendmail")
+;                ;;(setq message-send-mail-function `message-send-mail-with-sendmail))
+;                (t
+;                 (error
+;                  (concat "Don't know which mail server to use for "
+;                          from))))))))
+;(add-hook 'message-setup-hook 'fs-change-smtp)
 
 ;; 拼写检查
 (add-hook 'message-send-hook 'ispell-message)
@@ -100,7 +125,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; 地址簿
 
-(require 'bbdb)
+(require 'bbdb)     ; 需要安装 bbdb 软件包，否则加载会失败！
 (bbdb-initialize 'gnus 'message)
 
 ;; 不检查是否符合美国电话号码标准
@@ -123,8 +148,8 @@
 ;;; 杂项
 
 ;;设置标题行样式
-(setq gnus-group-line-format "%M%S%p%P%5y:%B%(%g%)%l %O\n")
-(setq gnus-summary-line-format ":%U%R %B %s %-100=|%3L|%-20,20n|%&user-date;\n")
+;(setq gnus-group-line-format "%M%S%p%P%5y:%B%(%g%)%l %O\n")
+;(setq gnus-summary-line-format ":%U%R %B %s %-100=|%3L|%-20,20n|%&user-date;\n")
 
 ;; 让邮件列表中的新邮件显示在上面
 (setq gnus-thread-sort-functions
@@ -150,4 +175,7 @@
                            "Content-Transfer-Encoding:\\|"
                            "^X-mailer:\\|^X-Newsreader:\\|^X-Sender:\\|"
                            gnus-visible-headers))))
+
+;; 设置信件自动删除的时间为一个月
+(setq nnmail-expiry-wait 30)
 
