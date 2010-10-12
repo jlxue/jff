f4df554f3ff967c6f82b182eb5c2257360025a12
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-10-12 15:40:46 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@875 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/cpan-search/cpan-search.pl
+++ Perl/cpan-search/cpan-search.pl
@@ -1,10 +1,92 @@
 #!/usr/bin/perl
+# Purpose:
+#   A more powerful cpan searcher than search.cpan.org
+#
+# Usage:
+#   Run `perl cpan-search.pl' for details.
+#
+# License:
+#   GPL v3
+#
+# Author:
+#   Yubao Liu <yubao.liu@gmail.com>
+#
+# ChangeLog
+#   2010-09-26  Yubao Liu
+#       * initial skeleton, implemented mirror_files()
+#
+#   2010-10-08  Yubao Liu
+#       * refactored with App::Cmd
 use strict;
 use warnings;
-use Data::Dumper;
+
+########################################################################
+{
+package CPANSearchApp;
+use strict;
+use warnings;
+use AppConfig;
+use App::Cmd::Setup -app;
+
+
+sub config {
+    my $app = shift;
+
+    if (! exists $app->{config}) {
+        my $config = AppConfig->new();
+        $app->{config} = $config;
+    }
+
+    return $app->{config};
+}
+
+1;
+}
+
+########################################################################
+{
+package CPANSearchApp::Command;
+use strict;
+use warnings;
+use App::Cmd::Setup -command;
+
+sub opt_spec {
+    my ( $class, $app ) = @_;
+    return (
+        [ 'help' => "This usage screen" ],
+        $class->options($app),
+    )
+}
+
+sub validate_args {
+    my ( $self, $opt, $args ) = @_;
+    die $self->_usage_text if $opt->{help};
+    $self->validate( $opt, $args );
+}
+
+1;
+}
+
+########################################################################
+{
+package CPANSearchApp::Command::update;
+use strict;
+use warnings;
 use File::Basename;
-use IO::Uncompress::AnyUncompress qw(anyuncompress $AnyUncompressError);
 use LWP::UserAgent;
+#use CPANSearchApp -command;
+CPANSearchApp->import(qw(-command));
+
+
+sub abstract {
+    "update CPAN related data files"
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+
+    print "Updating...\n";
+}
 
 sub mirror_files($@) {
     my ($site, @files) = @_;
@@ -29,6 +111,49 @@ sub mirror_files($@) {
         my $response = $ua->mirror($remote_file, $file);
         die "Failed to mirror $remote_file: " . $response->status_line if $response->is_error;
     }
+}
+
+######## MAIN ENTRY ##################################
+my @MIRROR_FILES = qw(
+    authors/00whois.html
+    authors/00whois.xml
+    authors/01mailrc.txt.gz
+    indices/du-k.gz
+    indices/find-ls.gz
+    indices/ls-lR.gz
+    modules/00modlist.long.html
+    modules/01modules.index.html
+    modules/01modules.mtime.html
+    modules/01modules.mtime.rss
+    modules/02packages.details.txt.gz
+    modules/03modlist.data.gz
+);
+
+my $MIRROR_SITE = 'http://cpan.wenzk.com/';
+
+#mirror_files($MIRROR_SITE, @MIRROR_FILES);
+
+#my ($meta_files, $author_id_map, $dists) = filter_find_ls($ARGV[0]);
+#print Dumper($author_id_map);
+1;
+}
+
+########################################################################
+{
+package CPANSearchApp::Command::build;
+use strict;
+use warnings;
+use IO::Uncompress::AnyUncompress qw(anyuncompress $AnyUncompressError);
+#use CPANSearchApp -command;
+CPANSearchApp->import(qw(-command));
+
+
+sub abstract {
+    "some abstract"
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
 }
 
 sub filter_find_ls($) {
@@ -57,26 +182,58 @@ sub filter_find_ls($) {
     return (\@meta_files, \%author_id_map, \@dists);
 }
 
-######## MAIN ENTRY ##################################
-my @MIRROR_FILES = qw(
-    authors/00whois.html
-    authors/00whois.xml
-    authors/01mailrc.txt.gz
-    indices/du-k.gz
-    indices/find-ls.gz
-    indices/ls-lR.gz
-    modules/00modlist.long.html
-    modules/01modules.index.html
-    modules/01modules.mtime.html
-    modules/01modules.mtime.rss
-    modules/02packages.details.txt.gz
-    modules/03modlist.data.gz
-);
+1;
+}
 
-my $MIRROR_SITE = 'http://cpan.wenzk.com/';
+########################################################################
+{
+package CPANSearchApp::Command::search;
+use strict;
+use warnings;
+#use CPANSearchApp -command;
+CPANSearchApp->import(qw(-command));
 
-#mirror_files($MIRROR_SITE, @MIRROR_FILES);
 
-my ($meta_files, $author_id_map, $dists) = filter_find_ls($ARGV[0]);
-print Dumper($author_id_map);
+sub abstract {
+    "some abstract"
+}
+
+sub execute {
+    my ($self, $opt, $args) = @_;
+}
+
+1;
+}
+
+########################################################################
+#   {
+#   package CPANSearchApp::Command::foo;
+#   use strict;
+#   use warnings;
+#   #use CPANSearchApp -command;
+#   CPANSearchApp->import(qw(-command));
+#
+#   
+#   sub abstract {
+#       "some abstract"
+#   }
+#   
+#   sub execute {
+#       my ($self, $opt, $args) = @_;
+#   }
+#   
+#   1;
+#   }
+
+########################################################################
+{
+package main;
+use strict;
+use warnings;
+#use CPANSearchApp;
+CPANSearchApp->import;
+
+
+CPANSearchApp->run;
+}
 
