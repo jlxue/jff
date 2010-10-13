52d533da93f90fccd7b1a5d5d00bc2a7aed7c178
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-10-13 18:37:54 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@891 1de5394f-e846-0410-8a38-9b14b2994ffe

---
+++ Lisp/lisp.el
@@ -0,0 +1,32 @@
+;; http://www.emacswiki.org/emacs/ParEdit
+(defvar electrify-return-match
+  "[\]}\)\"]"
+  "If this regexp matches the text after the cursor, do an \"electric\" return.")
+
+(defun electrify-return-if-match (arg)
+  "If the text after the cursor matches `electrify-return-match' then
+open and indent an empty line between the cursor and the text.  Move the
+cursor to the new line."
+  (interactive "P")
+  (let ((case-fold-search nil))
+    (if (looking-at electrify-return-match)
+        (save-excursion (newline-and-indent)))
+    (newline arg)
+    (indent-according-to-mode)))
+
+(autoload 'paredit-mode "paredit"
+  "Minor mode for pseudo-structurally editing Lisp code." t)
+(when (fboundp 'paredit-mode)
+  (mapc (lambda (hook)
+          (add-hook hook (lambda ()
+                           (paredit-mode +1)
+                           (local-set-key (kbd "RET") 'electrify-return-if-match)
+                           (show-paren-mode t))))
+        '(emacs-lisp-mode-hook lisp-mode-hook lisp-interaction-mode-hook slime-repl-mode-hook)))
+
+;; Stop SLIME's REPL from grabbing DEL,
+;; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+    (read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
