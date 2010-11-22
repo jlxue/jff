7da5d7f41445ac08188e4380e0d1d62e6e7e852a
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-11-22 15:51:30 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@915 1de5394f-e846-0410-8a38-9b14b2994ffe

--- bitsway/upnp.cpp
+++ bitsway/upnp.cpp
@@ -1,10 +1,55 @@
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include "upnp.hpp"
 
 namespace bitsway {
 
-UPnPIGDClient::UPnPIGDClient(boost::asio::io_service& io_service)
+using namespace boost::asio;
+
+static const char*      UPNP_MULTI_CAST_ADDR = "239.255.255.250";
+static unsigned short   UPNP_MULTI_CAST_PORT = 1900;
+static const char*      UPNP_IGD_SEARCH_REQUEST =
+    ""
+    ""
+    "";
+
+UPnPIGDClient::UPnPIGDClient(io_service& io_svc)
+    : multicast_address_(ip::address::from_string(UPNP_MULTI_CAST_ADDR)),
+      multicast_endpoint_(multicast_address_, UPNP_MULTI_CAST_PORT),
+      socket_(io_svc, multicast_endpoint_.protocol()),
+      timer_(io_svc)
 {
+    socket_.set_option(ip::udp::socket::reuse_address(true));
+    socket_.bind(ip::udp::endpoint());
+
+    socket_.async_send_to(buffer(UPNP_IGD_SEARCH_REQUEST),
+            multicast_endpoint_,
+            boost::bind(&UPnPIGDClient::handleSendTo,
+                this,
+                placeholders::error,
+                placeholders::bytes_transferred));
+
+    socket_.async_receive_from(buffer(data_, max_length),
+            server_endpoint_,
+            boost::bind(&UPnPIGDClient::handleReceiveFrom,
+                this,
+                placeholders::error,
+                placeholders::bytes_transferred));
+
+}
+
+void UPnPIGDClient::handleSendTo(const boost::system::error_code& ec,
+                                 std::size_t bytes_transferred)
+{
+    if (! ec) {
+    }
+}
+
+void UPnPIGDClient::handleReceiveFrom(const boost::system::error_code& ec,
+        std::size_t bytes_transferred)
+{
+    if (! ec) {
+    }
 }
 
 UPnPIGDClient::~UPnPIGDClient()
--- bitsway/upnp.hpp
+++ bitsway/upnp.hpp
@@ -9,6 +9,19 @@ class UPnPIGDClient {
 public:
     UPnPIGDClient(boost::asio::io_service& io_service);
     ~UPnPIGDClient();
+    void handleSendTo(const boost::system::error_code& ec,
+            std::size_t bytes_transferred);
+    void handleReceiveFrom(const boost::system::error_code& ec,
+            std::size_t bytes_transferred);
+
+private:
+    boost::asio::ip::address        multicast_address_;
+    boost::asio::ip::udp::endpoint  multicast_endpoint_;
+    boost::asio::ip::udp::endpoint  server_endpoint_;
+    boost::asio::ip::udp::socket    socket_;
+    boost::asio::deadline_timer     timer_;
+    enum { max_length = 1024 };
+    char                            data_[max_length];
 };
 
 } /* end namespace bitsway */
