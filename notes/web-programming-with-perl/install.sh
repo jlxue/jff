a7eeda9adc416960cdad69fd8f90d822c3cb6fed
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2010-11-22 15:58:47 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@930 1de5394f-e846-0410-8a38-9b14b2994ffe

--- notes/web-programming-with-perl/install.sh
+++ notes/web-programming-with-perl/install.sh
@@ -1,5 +1,6 @@
 # On Debian Squeeze, run with root privilege
 aptitude install apache2 apache2-mpm-prefork apache2-suexec \
-    libapache2-mod-fcgid libapache2-mod-perl2
+    libapache2-mod-fcgid libapache2-mod-perl2 \
+    libcgi-fast-perl libhtml-template-compiled-perl
 a2enmod userdir
 a2enmod suexec
