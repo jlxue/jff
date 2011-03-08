bc5a0ed2b68e359e1fcfe8670920cb4331030b03
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2011-03-08 14:06:12 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@974 1de5394f-e846-0410-8a38-9b14b2994ffe

---
+++ sysadmin/cfengine3/bootstrap.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -e
+
+adjust_cfengine3_dir () {
+    [ -L "$1" ] && rm "$1"
+    [ -d "$1" ] || mkdir -m 755 "$1"
+}
+
+adjust_cfengine3_dir /var/lib/cfengine3/bin
+adjust_cfengine3_dir /var/lib/cfengine3/inputs
+
+if [ ! -L "/etc/cfengine3" ]; then
+    rmdir /etc/cfengine3
+    ln -s /var/lib/cfengine3/inputs /etc/cfengine3
+fi
+
+# http://www.cfengine.org/pages/getting_started
+cf-key
+#cp -a /usr/sbin/cf-* /var/lib/cfengine3/bin
+cp `dirname "$0"`/inputs/*.cf /var/lib/cfengine3/inputs
+chmod 600 /var/lib/cfengine3/inputs/*.cf
+cf-agent --bootstrap
+cf-agent
+
+echo 'Successfully!'
+
