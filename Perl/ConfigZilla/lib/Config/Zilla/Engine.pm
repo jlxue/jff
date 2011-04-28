2c4378d8bf9ded881b91e72cb894fc28e5f3c065
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2011-04-28 18:01:27 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@1033 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/ConfigZilla/lib/Config/Zilla/Engine.pm
+++ Perl/ConfigZilla/lib/Config/Zilla/Engine.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Any::Moose;
 
-has ruleset     => (is => 'ro', isa => 'HashRef[Config::Zilla::Rule]');
+has 'ruleset'   => (is => 'ro', isa => 'HashRef[Config::Zilla::Rule]');
 
 sub addRule {
     my ($self, $rule) = @_;
@@ -26,6 +26,22 @@ sub validate {
     my ($self) = @_;
     my $rules = $self->ruleset;
 
+    return unless defined $rules;
+
+    while (my ($name, $rule) = each %$rules) {
+        $rule->validate();
+
+        my $depends = $rule->depend();
+        next unless defined $depends;
+
+        for my $dep (@$depends) {
+            confess "Rule \"$name\" depends non-exist rule \"$dep\"" unless
+                    exists $rules->{$dep};
+        }
+    }
+}
+
+sub run {
 }
 
 no Any::Moose;
