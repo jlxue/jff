80e07389e7f95dffc4d4848235ce951cec37fae1
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2011-04-28 17:56:40 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@1021 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/ConfigZilla/lib/Config/Zilla/Rule.pm
+++ Perl/ConfigZilla/lib/Config/Zilla/Rule.pm
@@ -3,10 +3,33 @@ use strict;
 use warnings;
 use Any::Moose;
 
-has 'name'      => (is => 'r', isa => 'Str', required => 1);
-has 'shortdesc' => (is => 'r', isa => 'Str', required => 1);
-has 'longdesc'  => (is => 'r', isa => 'Str');
-has 'depend'    => (is => 'r', isa => 'Str[]');
-has 'notify'    => (is => 'r', isa => 'Str[]');
+has 'name'      => (is => 'ro', isa => 'Str', required => 1);
+has 'shortdesc' => (is => 'ro', isa => 'Str', required => 1);
+has 'longdesc'  => (is => 'ro', isa => 'Str');
+has 'depend'    => (is => 'ro', isa => 'ArrayRef[Str]');
+has 'notify'    => (is => 'ro', isa => 'ArrayRef[Str]');
+has 'stash'     => (is => 'rw', isa => 'HashRef');
+
+sub needRepair {
+    return 1;
+}
+
+sub isRepaired {
+    return 0;
+}
+
+sub isKept {
+    return 0;
+}
+
+sub saveState {
+    my ($self, $isBeforeExecute) = @_;
+}
+
+sub execute {
+}
+
+sub rollback {
+}
 
 __PACKAGE__->meta->make_immutable();
