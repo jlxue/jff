97ce46b35ea419b5a20a4a9329e76966460f0b6c
Yubao.Liu <Yubao.Liu@1de5394f-e846-0410-8a38-9b14b2994ffe>
2011-04-28 17:58:22 +0000

    save
    git-svn-id: https://jff.googlecode.com/svn/trunk@1025 1de5394f-e846-0410-8a38-9b14b2994ffe

--- Perl/ConfigZilla/lib/Config/Zilla/Rule.pm
+++ Perl/ConfigZilla/lib/Config/Zilla/Rule.pm
@@ -6,10 +6,20 @@ use Any::Moose;
 has 'name'      => (is => 'ro', isa => 'Str', required => 1);
 has 'shortdesc' => (is => 'ro', isa => 'Str', required => 1);
 has 'longdesc'  => (is => 'ro', isa => 'Str');
-has 'target'    => (is => 'ro', isa => 'Str');
+
+# Depends on which rules
 has 'depend'    => (is => 'ro', isa => 'ArrayRef[Str]');
-has 'notify'    => (is => 'ro', isa => 'ArrayRef[Str]');
-has 'expect'    => (is => 'ro');
+
+# { ruleName1 => [ eventName, args ] }
+has 'notify'    => (is => 'ro', isa => 'HashRef');
+# { eventName => code }
+has 'listen'    => (is => 'ro', isa => 'HashRef');
+
+# Extra expectation
+has 'expect'    => (is => 'ro', isa => 'HashRef');
+
+# Only execute this rule after 'ifelapsed' seconds
+has 'ifelapsed' => (is => 'ro', isa => 'Int');
 
 no Any::Moose;
 __PACKAGE__->meta->make_immutable();
