e15b6f82b997095b72f20a08ee706458c51b7f3b
Liu Yubao <yubao.liu@gmail.com>
2011-09-10 21:27:10 +0800

    exclusive file lock

--- Perl/ConfigZilla/lib/Config/Zilla/FileLock.pm
+++ Perl/ConfigZilla/lib/Config/Zilla/FileLock.pm
@@ -2,35 +2,60 @@ package Config::Zilla::FileLock;
 use strict;
 use warnings;
 use utf8;
+use threads;
+use Log::Log4perl qw/:easy/;
 use POSIX qw/:fcntl_h/;
 
 sub new {
-    my ($class, $path) = @_;
+    my ($class, $path, $comment) = @_;
+    LOGCONFESS "Path not specified!" unless defined $path;
+    $comment = "" unless defined $comment;
+
+    my $self = {
+        path    => $path,
+        opened  => 0,
+        pid     => $$,
+        tid     => threads->tid,
+    };
 
     # XXX: not safe for NFS file system, see `perldoc -q lock`
-    if (sysopen my $fh, "$path", O_WRONLY | O_EXCL | O_CREAT, 0644) {
-        my $t = time();
+    # XXX: sysopen() may have 255 fd limit, see `perldoc -f sysopen`
+    my $fd;
+    if ($fd = POSIX::open($path, O_WRONLY | O_EXCL | O_CREAT, 0644)) {
+        $self->{opened} = 1;
 
-        syswrite $fh, "$$\nuid=$<, euid=$>, gid=$(, egid=$), time=$t\n" .
-            scalar(localtime($t)) . "\n";
+        my $t = time();
+        my $s = "$$\nuid=$<, euid=$>, gid=$(, egid=$), time=$t\n" .
+            scalar(localtime($t)) . "\n$comment\n";
+
+        POSIX::write($fd, $s, length($s));
+        POSIX::close($fd);
     } else {
-        $path = undef;
+        LOGCLUCK("Can't lock $path: $!");
     }
 
-    bless \$path, $class;
+    bless $self, $class;
 }
 
 sub locked {
     my ($self) = @_;
 
-    return defined $$self;
+    return $self->{opened} &&
+        $$ == $self->{pid} &&
+        threads->tid == $self->{tid};
+}
+
+sub path {
+    my ($self) = @_;
+
+    return $self->{path};
 }
 
 sub DESTROY {
     my ($self) = @_;
 
     if ($self->locked) {
-        unlink $$self;
+        unlink $self->{path};
     }
 }
 
