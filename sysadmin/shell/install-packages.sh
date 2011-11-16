1378305abd7f53b0ce25fa26a39bf476a066667c
Liu Yubao <yubao.liu@gmail.com>
2011-11-16 14:19:16 +0800

    save

--- sysadmin/shell/install-packages.sh
+++ sysadmin/shell/install-packages.sh
@@ -11,14 +11,25 @@ i () {
 ###########################################################
 i "?or(~prequired,~pstandard,~pimportant)"
 
+## manage /etc under git
 i git etckeeper
 
+## security
 i auditd
+i debsums aide samhain
+i chkrootkit rkhunter   # loop in unhide.rb
+i fail2ban snort
 
+## firewall
 i shorewall-init shorewall shorewall6
 
 
 ###########################################################
+APT_LISTBUGS_FRONTEND=none
+APT_LISTCHANGES_FRONTEND=none
+DEBIAN_FRONTEND=noninteractive
+export APT_LISTBUGS_FRONTEND APT_LISTCHANGES_FRONTEND DEBIAN_FRONTEND
+
 aptitude -q update
 
 aptitude -q install -o Dpkg::Options::=--force-confold \
