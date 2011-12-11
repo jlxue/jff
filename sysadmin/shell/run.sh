dc6691fe9d0d5be2127fc13fb29f31bd02665eb8
Liu Yubao <yubao.liu@gmail.com>
2011-12-12 02:23:23 +0800

    save

--- sysadmin/shell/run.sh
+++ sysadmin/shell/run.sh
@@ -7,6 +7,28 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin
 SCRIPT_DIR=$(readlink -f $(dirname $0))
 . $SCRIPT_DIR/lib.sh
 
+# fai.sh        fai-project.org
+# mta.sh        exim + smarthost, spamassassin/bogofilter/bsfilter, clamav
+# imap.sh       dovecot/cyrus-imapd
+# webmail.sh    squirrelmail, calendar
+# maillist.sh   sympa/mailman   (forum?cms?)
+# samba.sh
+# im.sh         ejabberd/openfire
+# wiki.sh       fosiwiki/moinmoin
+# cms.sh        drupal
+# blog.sh       wordpress
+# svn.sh        subversion + http, viewvc, reviewboard, Rietveld
+# git.sh        gitweb/cgit, gitolite, git-http-backend, gerrit
+# trac.sh
+# redmine.sh
+# bts.sh        bugzilla/mantis
+# ci.sh         jenkins
+# backup.sh     bacula/amanda
+# search.sh     apache solr, katta.sourceforge.net
+# monitor.sh    cacti, nagios, SEC(Simple Event Correlator), logcheck, fluentd, scribe
+#               monit
+# sugarCRM?, groupware?, zimbra?
+
 SCRIPTS=`cat<<END
 install-packages.sh
 locale.sh
