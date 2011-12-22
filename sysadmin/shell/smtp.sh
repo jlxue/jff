f42fe0b37c047119d75043a55c83478321ae2faa
Liu Yubao <yubao.liu@gmail.com>
2011-12-22 19:51:50 +0800

    save

--- sysadmin/shell/smtp.sh
+++ sysadmin/shell/smtp.sh
@@ -6,6 +6,11 @@ SCRIPT_DIR=$(readlink -f $(dirname $0))
 . $SCRIPT_DIR/lib.sh
 
 
+cmp_file $SCRIPT_DIR/etc/default/spamassassin /etc/default/spamassassin || {
+    overwrite_file $SCRIPT_DIR/etc/default/spamassassin /etc/default/spamassassin
+    service spamassassin restart
+}
+
 sync_file $SCRIPT_DIR/etc/aliases /etc/aliases
 sync_file $SCRIPT_DIR/etc/email-addresses /etc/email-addresses
 
@@ -26,6 +31,7 @@ cmp_dir $SCRIPT_DIR/etc/exim4 /etc/exim4 --exclude passwd.client {
 }
 
 
+ensure_mode_user_group /etc/default/spamassassin    644 root root
 ensure_mode_user_group /etc/exim4               755 root root
 ensure_mode_user_group /etc/exim4/conf.d        755 root root
 ensure_mode_user_group /etc/exim4/update-exim4.conf.conf    644 root root
@@ -35,6 +41,12 @@ ensure_mode_user_group /etc/mailname            644 root root
 ensure_mode_user_group /etc/aliases             644 root root
 ensure_mode_user_group /etc/email-addresses     644 root root
 
+
+[ "`pidof clamd`" ] || service clamav-daemon start
+
+[ "`pidof freshclam`" ] || service clamav-freshclam start
+
+[ "`pgrep spamd`" ] || service spamassassin start
 
 [ "`pidof exim4`" ] || service exim4 start
 
