1bd3da11d61a44cec92711869ad04dfa6843e110
Liu Yubao <yubao.liu@gmail.com>
2011-12-30 21:11:47 +0800

    save

--- sysadmin/shell/roundcube.sh
+++ sysadmin/shell/roundcube.sh
@@ -5,6 +5,8 @@ set -e -x
 SCRIPT_DIR=$(readlink -f $(dirname $0))
 . $SCRIPT_DIR/lib.sh
 
+#mkdir -p -m 0755 /srv/www/mail
+
 cmp_file $SCRIPT_DIR/etc/dbconfig-common/roundcube.conf /etc/dbconfig-common/roundcube.conf || {
     overwrite_file $SCRIPT_DIR/etc/dbconfig-common/roundcube.conf /etc/dbconfig-common/roundcube.conf
     CONF_CHANGED=1
@@ -21,6 +23,8 @@ ensure_mode_user_group /etc/dbconfig-common/roundcube.conf  600 root root
 ensure_mode_user_group /etc/roundcube                       755 root root
 ensure_mode_user_group /etc/roundcube/debian-db.php         640 root www-data
 ensure_mode_user_group /etc/roundcube/main.inc.php          640 root www-data
+#ensure_mode_user_group /srv/www                             755 root root
+#ensure_mode_user_group /srv/www/mail                        755 root root
 
 
 [ -z "$CONF_CHANGED" ] || service apache2 restart
