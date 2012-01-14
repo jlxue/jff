c57bfdfa4964a1dfe8e7ef35e9f516c2706f778e
Liu Yubao <yubao.liu@gmail.com>
2012-01-14 23:33:08 +0800

    setup password for managesieve and sieverules plugins

--- sysadmin/shell/roundcube.sh
+++ sysadmin/shell/roundcube.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=$(readlink -f $(dirname $0))
 #mkdir -p -m 0755 /srv/www/mail
 
 ######################################################################################
-## install http_auth_autologin plugin and setup Dovecot master user account
+## install http_auth_autologin plugin
 ##
 cmp_dir $SCRIPT_DIR/usr/share/roundcube/plugins/http_auth_autologin /usr/share/roundcube/plugins/http_auth_autologin || {
     overwrite_dir $SCRIPT_DIR/usr/share/roundcube/plugins/http_auth_autologin /usr/share/roundcube/plugins/http_auth_autologin
@@ -25,7 +25,6 @@ cmp_dir $SCRIPT_DIR/usr/share/roundcube/plugins/http_auth_autologin /usr/share/r
 mkdir -p -m 0755 /etc/roundcube/plugins/http_auth_autologin
 f=/etc/roundcube/plugins/http_auth_autologin/config.inc.php
 tmpl=/usr/share/roundcube/plugins/http_auth_autologin/config.inc.php.dist
-master_user=webmail
 dummy='@@IMAP_AUTH_MASTER_PASSWORD@@'
 imap_passwd=
 set +x
@@ -40,8 +39,16 @@ sed -e "s/$dummy/$imap_passwd/" $tmpl | diff -q $f - >/dev/null || {
     chown root:www-data $f
     CONF_CHANGED=1
 }
+set -x
+######################################################################################
 
-credential=$(doveadm pw -s DIGEST-MD5 -u webmail@corp.example.com -p "$passwd")
+
+######################################################################################
+## setup Dovecot master user account
+##
+set +x
+master_user=webmail
+credential=$(doveadm pw -s DIGEST-MD5 -u webmail@corp.example.com -p "$imap_passwd")
 masterdb=/etc/dovecot/master-users
 grep -q "^$master_user:$credential$" $masterdb 2>/dev/null || {
     sed -i -e "s/^$master_user:.*/$master_user:$credential/" $masterdb ||
@@ -49,6 +56,40 @@ grep -q "^$master_user:$credential$" $masterdb 2>/dev/null || {
     chmod 640 $masterdb
     chown root:dovecot $masterdb
     service dovecot restart
+}
+set -x
+######################################################################################
+
+
+######################################################################################
+## setup managesieve plugin
+##
+f=/etc/roundcube/plugins/managesieve/config.inc.php
+tmpl=$SCRIPT_DIR$f
+dummy='@@MANAGESIEVE_AUTH_PW@@'
+set +x
+sed -e "s/$dummy/$imap_passwd/" $tmpl | diff -q $f - >/dev/null || {
+    sed -e "s/$dummy/$imap_passwd/" $tmpl > $f
+    chmod 640 $f
+    chown root:www-data $f
+    CONF_CHANGED=1
+}
+set -x
+######################################################################################
+
+
+######################################################################################
+## setup sieverules plugin
+##
+f=/etc/roundcube/plugins/sieverules/config.inc.php
+tmpl=$SCRIPT_DIR$f
+dummy='@@SIEVERULES_AUTH_PW@@'
+set +x
+sed -e "s/$dummy/$imap_passwd/" $tmpl | diff -q $f - >/dev/null || {
+    sed -e "s/$dummy/$imap_passwd/" $tmpl > $f
+    chmod 640 $f
+    chown root:www-data $f
+    CONF_CHANGED=1
 }
 set -x
 ######################################################################################
@@ -70,10 +111,11 @@ sasldblistusers2 -f $sasldb 2>/dev/null | grep -q '^webmail@corp.example.com:' &
     chown root:Debian-exim $sasldb
 }
 f=/etc/roundcube/main.inc.php
-tmpl=$SCRIPT_DIR/etc/roundcube/main.inc.php
+tmpl=$SCRIPT_DIR$f
 dummy='@@SMTP_PASS@@'
-sed -e "s/$dummy/$smtp_passwd/" $tmpl | diff -q $f - >/dev/null || {
-    sed -e "s/$dummy/$smtp_passwd/" $tmpl > $f
+dummy2='@@IMAP_AUTH_PW@@'
+sed -e "s/$dummy/$smtp_passwd/" -e "s/$dummy2/$imap_passwd/" $tmpl | diff -q $f - >/dev/null || {
+    sed -e "s/$dummy/$smtp_passwd/" -e "s/$dummy2/$imap_passwd/" $tmpl > $f
     chmod 640 $f
     chown root:www-data $f
     CONF_CHANGED=1
@@ -87,8 +129,11 @@ cmp_file $SCRIPT_DIR/etc/dbconfig-common/roundcube.conf /etc/dbconfig-common/rou
     CONF_CHANGED=1
 }
 
-cmp_dir $SCRIPT_DIR/etc/roundcube /etc/roundcube --exclude http_auth_autologin --exclude main.inc.php || {
-    overwrite_dir $SCRIPT_DIR/etc/roundcube /etc/roundcube --exclude http_auth_autologin --exclude main.inc.php
+cmp_dir $SCRIPT_DIR/etc/roundcube /etc/roundcube --exclude http_auth_autologin \
+        --exclude managesieve --exclude sieverules --exclude main.inc.php || {
+    overwrite_dir $SCRIPT_DIR/etc/roundcube /etc/roundcube \
+        --exclude http_auth_autologin --exclude managesieve --exclude sieverules \
+        --exclude main.inc.php
     CONF_CHANGED=1
 }
 
