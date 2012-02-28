f262ea264c6be7af8ffe068417f9e24c474308f4
Liu Yubao <yubao.liu@gmail.com>
2012-02-28 18:40:18 +0800

    save

--- sysadmin/shell/davical.sh
+++ sysadmin/shell/davical.sh
@@ -5,10 +5,19 @@ set -e -x
 SCRIPT_DIR=$(readlink -f $(dirname $0))
 . $SCRIPT_DIR/lib.sh
 
+ensure_service_started postgresql postgres
+
+su postgres -c 'psql -c "" davical' 2>/dev/null || {
+    su postgres -c /usr/share/davical/dba/create-database.sh
+}
+
 cmp_dir $SCRIPT_DIR/etc/davical /etc/davical || {
     overwrite_dir $SCRIPT_DIR/etc/davical /etc/davical
     CONF_CHANGED=1
 }
+
+ensure_mode_user_group /etc/davical/administration.yml      640 root www-data
+ensure_mode_user_group /etc/davical/config.php              640 root www-data
 
 [ -z "$CONF_CHANGED" ] || service apache2 restart
 
