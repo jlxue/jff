6113d19002ed7eaab9909c2c453339da0a32aaa8
Liu Yubao <yubao.liu@gmail.com>
2012-03-02 14:28:43 +0800

    import original mantis configuration

---
+++ sysadmin/shell/etc/mantis/custom_strings_inc.php
@@ -0,0 +1,11 @@
+<?php
+    /* custom_strings_inc.php - Local configuration file
+       -----------------------------------------------------
+	In this file you can place your custom configuration to define: 
+	to define custom strings.
+	The advantage of defining them here is to provide a simple upgrade path, 
+	and avoid having to re-do the changes when upgrading to the next Mantis release.
+	The file is automatically detected and included by Mantis code.
+    */
+
+?>
