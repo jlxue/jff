6b604ff119857e5f517dec64226f9d4bf91400c7
Liu Yubao <yubao.liu@gmail.com>
2012-03-02 15:49:04 +0800

    Revert "Merge branch 'etc-orig'"
    This reverts commit 1256d54ee3320c8ce571dabfb99cf6a523716cf9, reversing
    changes made to 6a795688ca17572def02bf483122857825c5d379.

--- sysadmin/shell/etc/mantis/custom_functions_inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-    /* custom_functions_inc.php - Local configuration file
-       -------------------------------------------
-	Custom functions are used to extend the functionality of Mantis by integrating user written
-	functions into the issue processing at strategic places. This allows the system administrator 
-	to change the functionality without re-writing parts of the internals of the code.
-    */
-
-?>
