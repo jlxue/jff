12dad7787db1540c8a087d3e71572ed2d15649e8
Liu Yubao <yubao.liu@gmail.com>
2012-03-05 16:33:38 +0800

    set default timezone for php

--- sysadmin/shell/apache.sh
+++ sysadmin/shell/apache.sh
@@ -22,6 +22,22 @@ cmp_dir $SCRIPT_DIR/etc/modsecurity /etc/modsecurity || {
 #    CONF_CHANGED=1
 #}
 
+grep -q '^\s*date\.timezone\s*=' /etc/php5/cgi/php.ini || {
+    timezone=$(cat /etc/timezone)
+    [ "$timezone" ] && perl -i -ne "
+        if (/^\\s*;\\s*date\\.timezone\\s*=\\s*\\S/) {
+            s/^\\s*;\\s*//;
+        } elsif (/^\\s*;\\s*date\\.timezone\\s*=/) {
+            print \"date.timezone = $timezone\n\";
+            next;
+        }
+
+        print;
+        " /etc/php5/cgi/php.ini
+
+    CONF_CHANGED=1
+}
+
 [ -z "$CONF_CHANGED" ] || service apache2 restart
 
 ensure_service_started apache2 apache2
