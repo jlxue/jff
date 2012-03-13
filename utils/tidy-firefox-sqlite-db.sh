321e43b24549530c2e5926a5df9392725af314ae
Liu Yubao <yubao.liu@gmail.com>
2012-03-13 19:39:40 +0800

    save

--- utils/tidy-firefox-sqlite-db.sh
+++ utils/tidy-firefox-sqlite-db.sh
@@ -3,27 +3,32 @@
 set -e
 
 tidy_places_moz_historyvisits () {
-    echo tidy_places_moz_historyvisits "$@"
+    true
 }
 
+tidy_places_moz_places () {
+    true
+}
 
-while read f table; do
-    db=$(basename $f .sqlite | tr - _)
+tidy_urlclassifier3_moz_classifier () {
+    true
+}
 
-    n=$(sqlite3 $f "select count(*) from $table")
-    printf "%-20s %6d   %s\n" $table $n $f
+find ~/.mozilla -type f -name "*.sqlite" | while read f; do
+    sqlite3 $f ".tables" | sed -e 's/ \+/\n/g' | while read table; do
+        [ -z "$table" ] || {
+            db=$(basename $f .sqlite | tr - _)
 
-    if expr "$n" ">=" 10000 >/dev/null; then
-        if [ x$(type -t "tidy_${db}_$table") = "xfunction" ]; then
-            "tidy_${db}_$table" $f $table
-        else
-            echo "WARN: no tidy function for table $table in $f" >&2
-        fi
-    fi
-done < <(
-    find ~/.mozilla -type f -name "*.sqlite" | while read f; do
-        sqlite3 $f ".tables" | sed -e 's/ \+/\n/g' | while read table; do
-            [ -z "$table" ] || echo "$f" "$table"
-        done
+            n=$(sqlite3 $f "select count(*) from $table")
+            printf "%-20s %6d   %s\n" $table $n $f
+
+            if expr "$n" ">=" 10000 >/dev/null; then
+                if [ x$(type -t "tidy_${db}_$table") = "xfunction" ]; then
+                    "tidy_${db}_$table" $f $table
+                else
+                    echo "WARN: no tidy function for table $table in $f" >&2
+                fi
+            fi
+        }
     done
-)
+done
