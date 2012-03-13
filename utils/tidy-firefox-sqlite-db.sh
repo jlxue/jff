63f527adb4e3bb27fdae97a3412ce0fa31ac5453
Liu Yubao <yubao.liu@gmail.com>
2012-03-13 22:20:01 +0800

    save

--- utils/tidy-firefox-sqlite-db.sh
+++ utils/tidy-firefox-sqlite-db.sh
@@ -2,8 +2,16 @@
 
 set -e
 
+if pgrep firefox >/dev/null; then
+    echo "Firefox is running, please quit it."
+    exit 1
+fi
+
 tidy_places_moz_historyvisits () {
-    true
+    local f="$1" table="$2"
+
+    sqlite3 "$f" \
+        "delete from $table where datetime(visit_date/1000000, \"unixepoch\") > datetime(\"now\", \"-1 month\")"
 }
 
 tidy_places_moz_places () {
@@ -22,13 +30,13 @@ find ~/.mozilla -type f -name "*.sqlite" | while read f; do
             n=$(sqlite3 $f "select count(*) from $table")
             printf "%-20s %6d   %s\n" $table $n $f
 
-            if expr "$n" ">=" 10000 >/dev/null; then
-                if [ x$(type -t "tidy_${db}_$table") = "xfunction" ]; then
-                    "tidy_${db}_$table" $f $table
-                else
-                    echo "WARN: no tidy function for table $table in $f" >&2
-                fi
+            if [ x$(type -t "tidy_${db}_$table") = "xfunction" ]; then
+                "tidy_${db}_$table" $f $table
+            else
+                echo "WARN: no tidy function for table $table in $f" >&2
             fi
+
+            sqlite3 "$f" VACUUM
         }
     done
 done
