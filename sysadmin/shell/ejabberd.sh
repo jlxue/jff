0e374627e0afac422b2b7d93176c211b3e637fa2
Liu Yubao <yubao.liu@gmail.com>
2012-03-18 03:11:48 +0800

    save

--- sysadmin/shell/ejabberd.sh
+++ sysadmin/shell/ejabberd.sh
@@ -16,11 +16,13 @@ cmp_dir $SCRIPT_DIR/etc/ejabberd /etc/ejabberd --exclude ejabberd.pem || {
     CONF_CHANGED=1
 }
 
-ensure_mode_user_group /etc/default/ejabberd    644 root root
-ensure_mode_user_group /etc/ejabberd            750 root ejabberd
+ensure_mode_user_group /etc/default/ejabberd        644 root root
+ensure_mode_user_group /etc/ejabberd                750 root ejabberd
 ensure_mode_user_group /etc/ejabberd/ejabberd.cfg   600 ejabberd ejabberd
 ensure_mode_user_group /etc/ejabberd/ejabberd.pem   640 root ejabberd
 ensure_mode_user_group /etc/ejabberd/inetrc         644 root root
+ensure_mode_user_group /var/log/ejabberd            2750 ejabberd adm
+ensure_mode_user_group /var/lib/ejabberd            700 ejabberd ejabberd
 
 
 [ -z "$CONF_CHANGED" ] || service ejabberd restart
