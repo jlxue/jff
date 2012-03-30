d6c1847b3d49c1100bb8b21ec4fd7c18f5874b12
Liu Yubao <yubao.liu@gmail.com>
2012-03-30 15:02:07 +0800

    save

--- sysadmin/shell/dns.sh
+++ sysadmin/shell/dns.sh
@@ -24,6 +24,8 @@ cmp_file $SCRIPT_DIR/etc/resolvconf/resolv.conf.d/tail /etc/resolvconf/resolv.co
 # Kerberos and LDAP are critical services, avoid resolving to Internet sites
 # by mistake in case internal DNS server is down.
 sync_file $SCRIPT_DIR/etc/hosts /etc/hosts
+sync_file $SCRIPT_DIR/etc/hostname /etc/hostname
+[ "`cat /etc/hostname`" = "`hostname`" ] || hostname "`cat /etc/hostname`"
 
 
 # bind9
@@ -45,6 +47,7 @@ cmp_dir $SCRIPT_DIR/etc/bind /etc/bind --exclude rndc.key || {
 ensure_mode_user_group /etc/resolvconf/resolv.conf.d/head   644 root root
 ensure_mode_user_group /etc/resolvconf/resolv.conf.d/tail   644 root root
 ensure_mode_user_group /etc/hosts           644 root root
+ensure_mode_user_group /etc/hostname        644 root root
 
 ensure_mode_user_group /etc/bind            2755 root bind
 ensure_mode_user_group /etc/bind/bind.keys   644 root bind
