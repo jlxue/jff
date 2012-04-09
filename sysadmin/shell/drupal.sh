613f364d502b39413f1c1a029ad01c25be6f8c8c
Liu Yubao <yubao.liu@gmail.com>
2012-04-09 23:31:02 +0800

    kerberos auth for Drupal 7

--- sysadmin/shell/drupal.sh
+++ sysadmin/shell/drupal.sh
@@ -12,23 +12,23 @@ cmp_dir $SCRIPT_DIR/etc/drupal/7 /etc/drupal/7 --exclude dbconfig.php || {
 }
 
 
-#mkdir -m 755 -p /usr/local/share/drupal/modules
-#mkdir -m 755 -p /usr/local/share/drupal/themes
-#
-#cmp_dir $SCRIPT_DIR/contrib/Drupal/webserver_auth /usr/local/share/drupal/modules/webserver_auth || {
-#    overwrite_dir $SCRIPT_DIR/contrib/Drupal/webserver_auth /usr/local/share/drupal/modules/webserver_auth
-#    CONF_CHANGED=1
-#}
-#
-#[ -e /usr/share/drupal7/modules/local ] || {
-#    ln -s /usr/local/share/drupal/modules /usr/share/drupal7/modules/local
-#    CONF_CHANGED=1
-#}
-#
-#[ -e /usr/share/drupal7/themes/local ] || {
-#    ln -s /usr/local/share/drupal/themes /usr/share/drupal7/themes/local
-#    CONF_CHANGED=1
-#}
+mkdir -m 755 -p /usr/local/share/drupal/modules
+mkdir -m 755 -p /usr/local/share/drupal/themes
+
+cmp_dir $SCRIPT_DIR/contrib/Drupal/webserver_auth /usr/local/share/drupal/modules/webserver_auth || {
+    overwrite_dir $SCRIPT_DIR/contrib/Drupal/webserver_auth /usr/local/share/drupal/modules/webserver_auth
+    CONF_CHANGED=1
+}
+
+[ -e /usr/share/drupal7/modules/local ] || {
+    ln -s /usr/local/share/drupal/modules /usr/share/drupal7/modules/local
+    CONF_CHANGED=1
+}
+
+[ -e /usr/share/drupal7/themes/local ] || {
+    ln -s /usr/local/share/drupal/themes /usr/share/drupal7/themes/local
+    CONF_CHANGED=1
+}
 
 
 ######################################################################################
