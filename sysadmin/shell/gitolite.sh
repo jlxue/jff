efcc87a0f242450fea115bc522f2fb83d4ec5638
Liu Yubao <yubao.liu@gmail.com>
2012-05-18 18:12:00 +0800

    make gitweb also run under suexec

--- sysadmin/shell/gitolite.sh
+++ sysadmin/shell/gitolite.sh
@@ -17,12 +17,17 @@ mkdir -m 755 -p /usr/local/share /usr/local/bin
 mkdir -m 755 -p /srv/www/git
 sync_file $SCRIPT_DIR/srv/www/git/gitolite-shell-wrapper /srv/www/git/gitolite-shell-wrapper
 
+# SuexecUserGroup is virtualhost wise, so I have to make gitweb
+# suexec-ed as user "git", this is not safe but I have no better
+# way except using a seperate virtual host.
+sync_file /usr/share/gitweb/gitweb.cgi  /srv/www/git/gitweb.cgi
+
 sync_file $SCRIPT_DIR/srv/git/gitolite.rc /srv/git/.gitolite.rc
 
 sync_file $SCRIPT_DIR/etc/gitweb.conf /etc/gitweb.conf
 
 
-ensure_mode_user_group /srv/git                 750 git git
+ensure_mode_user_group /srv/git                 700 git git
 ensure_mode_user_group /srv/git/.gitolite       755 git git
 ensure_mode_user_group /srv/git/.gitolite.rc    644 git git
 ensure_mode_user_group /srv/git/projects.list   644 git git
@@ -35,6 +40,7 @@ ensure_mode_user_group /srv/git/.ssh            700 git git
 # so this directory must be readable for other users.
 ensure_mode_user_group /srv/www/git     755 git git
 ensure_mode_user_group /srv/www/git/gitolite-shell-wrapper  700 git git
+ensure_mode_user_group /srv/www/git/gitweb.cgi              700 git git
 
 ensure_mode_user_group /etc/gitweb.conf         644 root root
 
