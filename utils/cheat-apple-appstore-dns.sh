9299389e73367f277d52bf7b99f7efcb25ce0cf7
Liu Yubao <yubao.liu@gmail.com>
2012-10-15 13:56:44 +0800

    add smarthosts.google.com

--- utils/cheat-apple-appstore-dns.sh
+++ utils/cheat-apple-appstore-dns.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
 # See http://dns.v2ex.com/  DNS server: 199.91.73.222, 178.79.131.110
+# See https://smarthosts.googlecode.com/svn/trunk/mobile_devices/iOS/hosts
+#
+# IP for a*.phobos.apple.com:
+#   203.78.36.40
+#   203.78.36.42
+#   219.76.10.14
 
 sudo dnsmasq -h -r `pwd`/appstore-resolv.conf -H `pwd`/appstore-hosts.txt -q -8 /tmp/dnsmasq.log -b -D -C /dev/null
 
